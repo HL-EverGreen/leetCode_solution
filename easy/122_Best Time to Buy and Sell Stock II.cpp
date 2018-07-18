efa2db7204cd2c717eb46c82fd5f11412f367665
HL-EverGreen <lhr449307680@gmail.com>
2018-07-18 22:33:41 +0800

    easy

---
+++ easy/122_Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) { //sell immediately if price get higher
+        int res=0;
+        if(prices.size()<=1) return res;
+        for(int i=0;i<prices.size()-1;i++){
+            if(prices[i+1]>prices[i])
+                res+=prices[i+1]-prices[i];
+        }
+        return res;
+    }
+};
