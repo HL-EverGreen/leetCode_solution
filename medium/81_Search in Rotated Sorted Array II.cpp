53cfdee9c9bdd01b77a681e9aa8cc2b14832211d
HL-EverGreen <lhr449307680@gmail.com>
2018-08-24 23:28:07 -0400

    binary_search
    binary_search

---
+++ medium/81_Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {//binary search
+        int low=0,high=nums.size()-1,mid;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(nums[mid]==target) return true;
+            if(nums[mid]==nums[low] && nums[mid]==nums[high]){low++;high--;} //can't decide go left or go right under this condition, so we just reduce the range
+            else if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<nums[mid]) high=mid-1;
+                else low=mid+1;
+            }
+            else{
+                if(nums[high]>=target && target>nums[mid]) low=mid+1;
+                else high=mid-1;
+            }
+        }
+        return false;
+    }
+};
