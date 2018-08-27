a7cdffe53203552e3c4278f4fc6de9ebbd2a2831
HL-EverGreen <lhr449307680@gmail.com>
2018-08-27 17:44:10 -0400

    binary tree recursive
    binary tree recursive

---
+++ medium/236_Lowest Common Ancestor of a Binary Tree.cpp
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {//binary tree recursive
+        if(!root || root==p || root==q) return root;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        return !left?right:!right?left:root; //如果p,q在root两边被找到，则返回root，若只有一边发现了p、q，则返回该边返回的值
+    }
+};
