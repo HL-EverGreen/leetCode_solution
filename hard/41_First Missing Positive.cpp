ae9ff6b21d80e0386446da9f6b4f9295c5b3e93f
HL-EverGreen <lhr449307680@gmail.com>
2018-09-14 15:23:35 -0400

    number_in_right_position
    number_in_right_position

---
+++ hard/41_First Missing Positive.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) { //number_in_right_position
+        //Put each number in its right place.
+        //For example:
+        //When we find 5, then swap it with A[4].
+        //At last, the first place where its number is not right, return the place + 1.
+        int n_size=nums.size();
+        for(int i=0;i<n_size;i++)
+            while(nums[i]>0 && nums[i]<=n_size && nums[nums[i]-1]!=nums[i]) swap(nums[i], nums[nums[i]-1]);
+        
+        for(int i=0;i<n_size;i++)
+            if(nums[i]!=i+1) return i+1;
+        
+        return n_size+1;
+    }
+};
