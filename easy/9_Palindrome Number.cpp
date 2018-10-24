1276854c0c256f87df5512e1bf9c83afe5f0f22f
HL-EverGreen <lhr449307680@gmail.com>
2018-10-23 22:20:37 -0400

    int_palindrome
    int_palindrome

--- easy/9_Palindrome Number.cpp
+++ easy/9_Palindrome Number.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
-    bool isPalindrome(int x) { //two directions
+    bool isPalindrome(int x) { //int palindrome
+        //reverse the input x and compare
+        if(x < 0) return false;
+        int old = x, reverse = 0;
+        while(x > 0){
+            reverse = 10 * reverse + x % 10;
+            x /= 10;
+        }
+        return reverse == old;
+        
+        //reverse the second half and compare
+        /*
+        if(x < 0 || (x != 0 && x % 10 == 0)) return false;
+        int sum = 0;
+        while(x > sum){
+            sum = sum * 10 + x % 10;
+            x /= 10;
+        }
+        return x == sum || x == sum / 10;
+        */
+
+        //use to_string
+        /*
         if(x<0) return false;
         string num=to_string(x);
         int left=0,right=num.length()-1;
@@ -10,5 +32,6 @@ public:
             right--;
         }
         return true;
+        */
     }
 };
