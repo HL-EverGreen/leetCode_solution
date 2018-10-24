e1ff27593f7feb6803b612bbfcf352571e7700e3
HL-EverGreen <lhr449307680@gmail.com>
2018-10-23 22:08:25 -0400

    Update medium/6_ZigZag Conversion.cpp
    string

--- medium/6_ZigZag Conversion.cpp
+++ medium/6_ZigZag Conversion.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     string convert(string s, int numRows) { //string
+        //use vector
+        if(s.size() == 0 || numRows <= 0) return "";
+        if(numRows == 1) return s;
+        vector<string> rcv(numRows, "");
+        int cur = 0;
+        bool flag = true;
+        for(char ch:s){
+            rcv[cur] += ch;
+            if(cur == 0) flag = true;
+            if(cur == numRows - 1) flag = false;
+            if(flag) cur++;
+            else cur--;
+        }
+        string res;
+        for(auto elem:rcv) res += elem;
+        return res;
+
+        //use new
+        /*
         if(numRows<=1) return s;
         const int len=s.length();
         string *str=new string[numRows], res;
@@ -16,5 +35,6 @@ public:
         for(int i=0;i<numRows;i++) res.append(str[i]);
         delete[] str; //avoid memory leak
         return res;
+        */
     }
 };
