d04e11c7aebedc51fd701435ce1a4ddd5e5fc07d
HL-EverGreen <lhr449307680@gmail.com>
2018-10-23 21:41:27 -0400

    hash_map
    hash_map

--- easy/1_Two Sum.cpp
+++ easy/1_Two Sum.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target) { //hash_map
+        //use hash_map, O(n)
+        int size = nums.size();
+        vector<int> res;
+        if(size <= 1) return res;
+        unordered_map<int, int> myMap;
+        for(int i = 0; i < size; i++){
+            if(myMap.find(target - nums[i]) != myMap.end()){
+                res.push_back(myMap[target - nums[i]]);
+                res.push_back(i);
+                return res;
+            }
+            myMap[nums[i]] = i;
+        }
+
+        //O(n^2);
+        /*
         vector<int> res(2);
         for(int i = 0; i < nums.size()-1;i++)
         {
@@ -14,5 +30,6 @@ public:
                 }
             }
         }
+        */
     }
 };
