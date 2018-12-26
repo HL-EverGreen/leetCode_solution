1deca35d0596e341faf4f4fa10925e9639d4c773
HL-EverGreen <lhr449307680@gmail.com>
2018-12-25 21:50:15 -0500

    array_DP
    array_DP

--- medium/63_Unique Paths II.cpp
+++ medium/63_Unique Paths II.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) { //dp
-        //dp[i][j] presents block(i,j)'s number of unique paths, here I change obstacleGrid directly instead of defining new dp[][]
-        //the main idea is that dp[i][j]=dp[i+1][j]+dp[i][j+1]; if dp[i+1][j] or dp[i][j+1] has obstacle, just use 0 when add up
-        int row=obstacleGrid.size(), col=row?obstacleGrid[0].size():0;
-        if(col==0) return 0;
-        for(int id_row=row-1;id_row>=0;id_row--){
-            for(int id_col=col-1;id_col>=0;id_col--){
-                if(obstacleGrid[id_row][id_col]==1){
-                    if((id_row==row-1 && id_col==col-1) || (id_row==0 && id_col==0)) return 0; //obstacle located in start point or end point
-                    obstacleGrid[id_row][id_col]=-1; //use -1 to present obstacle instead of 1
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        // array & DP
+        // time complexity: O(n^2)
+        // 0ms, beats 100.00%
+        
+        // the main idea is that dp[i][j]=dp[i+1][j]+dp[i][j+1]; if dp[i+1][j] or dp[i][j+1] has obstacle, just use 0 when add up
+        int row = obstacleGrid.size(), col = (row == 0 ? 0 : obstacleGrid[0].size());
+        if(col == 0) { return 0; }
+        // check if obstacle located in start point or end point
+        if(obstacleGrid[row - 1][col - 1] == 1 || obstacleGrid[0][0] == 1) { return 0; }
+        
+        for(int cur_row = row - 1; cur_row >= 0; cur_row--) {
+            for(int cur_col = col - 1; cur_col >= 0; cur_col--) {
+                if(obstacleGrid[cur_row][cur_col] == 1) {
+                    obstacleGrid[cur_row][cur_col] = -1; //use -1 to present obstacle instead of 1
                     continue;
                 }
-                if(id_col==col-1 && id_row==row-1) obstacleGrid[id_row][id_col]=1;
-                else if(id_col==col-1) obstacleGrid[id_row][id_col]+=obstacleGrid[id_row+1][id_col]==-1?0:obstacleGrid[id_row+1][id_col];
-                else if(id_row==row-1) obstacleGrid[id_row][id_col]+=obstacleGrid[id_row][id_col+1]==-1?0:obstacleGrid[id_row][id_col+1];
-                else obstacleGrid[id_row][id_col]+=(obstacleGrid[id_row][id_col+1]==-1?0:obstacleGrid[id_row][id_col+1])+(obstacleGrid[id_row+1][id_col]==-1?0:obstacleGrid[id_row+1][id_col]);
+                if(cur_row == row - 1 && cur_col == col - 1) { 
+                    obstacleGrid[cur_row][cur_col] = 1; 
+                } else if(cur_row == row - 1) { 
+                    obstacleGrid[cur_row][cur_col] = (obstacleGrid[cur_row][cur_col + 1] == -1 ? 0 : obstacleGrid[cur_row][cur_col + 1]); 
+                } else if(cur_col == col - 1) { 
+                    obstacleGrid[cur_row][cur_col] = (obstacleGrid[cur_row + 1][cur_col] == -1 ? 0 : obstacleGrid[cur_row + 1][cur_col]); 
+                } else { 
+                    obstacleGrid[cur_row][cur_col] = (obstacleGrid[cur_row][cur_col + 1] == -1 ? 0 : obstacleGrid[cur_row][cur_col + 1]) + (obstacleGrid[cur_row + 1][cur_col] == -1 ? 0 : obstacleGrid[cur_row + 1][cur_col]); 
+                }
             }
         }
         return obstacleGrid[0][0];
