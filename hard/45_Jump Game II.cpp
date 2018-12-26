a6be98287fe5458f789478a718433f1db8664478
HL-EverGreen <lhr449307680@gmail.com>
2018-12-25 20:38:59 -0500

    array_BFS
    array_BFS

--- hard/45_Jump Game II.cpp
+++ hard/45_Jump Game II.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {  //bfs
-        int n_size=nums.size(), step=0, start=0, end=0;
-        while(end<n_size-1){
+    int jump(vector<int>& nums) {
+        // array & BFS
+        // Time complexity: O(n)
+        // each round compute new range which can be reached in one move from the current range
+        
+        // start and end is the current range in this round
+        int n_size = nums.size(), step = 0, start = 0, end = 0;
+        while(end < n_size - 1) {
             step++;
-            int maxend=end+1;
-            for(int i=start;i<=end;i++){
-                if(i+nums[i]>=n_size-1) return step;
-                maxend=max(maxend, i+nums[i]);
+            int cur_max_end = end; 
+            for(int i = start; i <= end; i++) {
+                if(nums[i] + i >= n_size - 1) {
+                    return step;
+                }
+                cur_max_end = max(cur_max_end, nums[i] + i);
             }
-            start=end+1;
-            end=maxend;
+            start = end + 1;
+            end = cur_max_end;
         }
+        // handle case of input nums's size == 0 or == 1
         return step;
     }
 };
