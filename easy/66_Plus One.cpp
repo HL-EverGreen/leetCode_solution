41679c30c125b9f2747ccf9f2dbb968841e7157b
HL-EverGreen <lhr449307680@gmail.com>
2018-12-25 23:04:49 -0500

    array
    array

--- easy/66_Plus One.cpp
+++ easy/66_Plus One.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        for(int i=digits.size()-1;i>=0;i--){
-            if(digits[i]==9){
-                digits[i]=0;
-            }
-            else{
+        // array
+        // time complexity: O(n)
+        // if all number is 9, set first digit to 1 and others to 0, then push one more 0 to the vector
+        
+        for(int i = digits.size() - 1; i >= 0; i--) {
+            if(digits[i] == 9) {
+                digits[i] = 0;
+            } else {
                 digits[i]++;
                 return digits;
             }
         }
-        digits[0]=1;
+        digits[0] = 1;
         digits.push_back(0);
         return digits;
     }
