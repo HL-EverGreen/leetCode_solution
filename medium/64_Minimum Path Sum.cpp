bf6c7ca14189cba3ab8d7a279f96e833394be6bf
HL-EverGreen <lhr449307680@gmail.com>
2018-12-25 22:01:51 -0500

    array_DP
    array_DP

--- medium/64_Minimum Path Sum.cpp
+++ medium/64_Minimum Path Sum.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int row=grid.size();
-        if(grid.size()==0||grid[0].size()==0)
-            return 0;
-        vector<vector<int>> res(grid.size(),vector<int>(grid[0].size(),0));
-        res[grid.size()-1][grid[0].size()-1]=grid[grid.size()-1][grid[0].size()-1];
-        dp(grid,res,grid.size(),grid[0].size());
-        return res[0][0];
-    }
-    
-    void dp(vector<vector<int>> grid, vector<vector<int>>& res, int row, int col){
-        for(int n=col-1;n>=0;n--){
-            for(int m=row-1;m>=0;m--){
-                if(n==col-1 && m==row-1)
+        // array & DP
+        // time complexity: O(n^2)
+        // 4ms, beats 100.00%
+        
+        // in-place modify, grid[i][j] += min(grid[i + 1][j], grid[i][j + 1])
+        int row = grid.size(), col = (row == 0 ? 0 : grid[0].size());
+        if(col == 0) return -1;
+        
+        for(int cur_row = row - 1; cur_row >= 0; cur_row--) {
+            for(int cur_col = col - 1; cur_col >= 0; cur_col--) {
+                if(cur_row == row - 1 && cur_col == col - 1) {
                     continue;
-                else if(n==col-1)
-                    res[m][n]=res[m+1][n]+grid[m][n];
-                else if(m==row-1)
-                    res[m][n]=res[m][n+1]+grid[m][n];
-                else
-                    res[m][n]=min(res[m+1][n],res[m][n+1])+grid[m][n];
+                } else if(cur_row == row - 1) {
+                    grid[cur_row][cur_col] += grid[cur_row][cur_col + 1];
+                } else if(cur_col == col - 1) {
+                    grid[cur_row][cur_col] += grid[cur_row + 1][cur_col];
+                } else {
+                    grid[cur_row][cur_col] += min(grid[cur_row + 1][cur_col], grid[cur_row][cur_col + 1]);
+                }
             }
         }
+        return grid[0][0];
     }
 };
