8b0a8167194d83a616f35078b8b086f909574ef5
HL-EverGreen <lhr449307680@gmail.com>
2018-12-27 14:12:03 -0500

    array_binary_search
    array_binary_search

--- hard/154_Find Minimum in Rotated Sorted Array II.cpp
+++ hard/154_Find Minimum in Rotated Sorted Array II.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-    int findMin(vector<int>& nums) {  //binary search
-        if(nums.size()==1) return nums[0];
-        int left=0,right=nums.size()-1;
-        while(left<right){
-            if(nums[left]<nums[right])
+    int findMin(vector<int>& nums) {
+        // array & binary search
+        // time complexity: O(logn)
+        // 4ms, beats 99.04%
+        
+        // binary search case:
+        // case 1: nums[left] < nums[right], nums[left] is the minimal number
+        // case 2: nums[mid] < nums[right], search left
+        // case 3: nums[mid] > nums[right], search right
+        // case 4: nums[mid] == nums[right], can't determine search left or right, so decrease search range (right--)
+        int left = 0, right = nums.size() - 1;
+
+        while(left < right) {
+            if(nums[left] < nums[right]) {          // case 1
                 return nums[left];
-            int mid=(left+right)/2;
-            if(nums[mid]>nums[right])
-                left=mid+1;
-            else if(nums[mid]<nums[right]) right=mid;
-            else right--;  //We can't determine whether to move right or left when nums[mid]==nums[right], so just let upper bound reduce one
+            }
+            int mid = left + (right - left) / 2;
+            
+            if(nums[mid] > nums[right]) {           // case 3
+                left = mid + 1;
+            } else if(nums[mid] < nums[right]) {    // case 2
+                right = mid;
+            } else { right --; }                    // case 4
         }
         return nums[left];
     }
