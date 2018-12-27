74a12eaeb4c15bba90f07efcf5a4435600b5babf
HL-EverGreen <lhr449307680@gmail.com>
2018-12-27 16:10:53 -0500

    array_binary_search_tree
    array_binary_search_tree

--- medium/105_Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ medium/105_Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -8,35 +8,34 @@
  * };
  */
 class Solution {
-
 public:
-    /* from Preorder and Inorder Traversal */
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-
-        return helper(preorder,0,preorder.size(),inorder,0,inorder.size());
-    
-    }
-
-    TreeNode* helper(vector<int>& preorder,int root_index,int pend,vector<int>& inorder,int istart,int iend)
-    {
+        // array & binary search tree
+        // time complexity: O(n)
+        // 8ms, beats 97.48%
+        
         // tree        8 4 5 3 7 3
         // preorder    8 [4 3 3 7] [5]
         // inorder     [3 3 4 7] 8 [5]
 
         // 每次从 preorder 头部取一个值 mid，作为树的根节点
         // 检查 mid 在 inorder 中 的位置，则 mid 前面部分将作为 树的左子树，右部分作为树的右子树
-
-        if(root_index >= pend || istart >= iend)
+        return buildTree(preorder, 0, preorder.size(), inorder, 0, inorder.size());
+    }
+    
+    TreeNode* buildTree(vector<int>& preorder, int root_index, int p_end, vector<int>& inorder, int i_start, int i_end) {
+        if(root_index >= p_end || i_start >= i_end) {
             return NULL;
-
+        }
+        
+        // current tree's root value
         int mid = preorder[root_index];
-        auto f = find(inorder.begin() + istart,inorder.begin() + iend,mid);
-
-        int dis = f - inorder.begin() - istart;
-
+        auto pos = find(inorder.begin() + i_start, inorder.begin() + i_end, mid);
+        int dis = pos - inorder.begin() - i_start;
+        
         TreeNode* root = new TreeNode(mid);
-        root -> left = helper(preorder,root_index + 1,root_index + 1 + dis,inorder,istart,istart + dis);
-        root -> right = helper(preorder,root_index + 1 + dis,pend,inorder,istart + dis + 1,iend);
+        root->left = buildTree(preorder, root_index + 1, root_index + dis + 1, inorder, i_start, i_start + dis);
+        root->right = buildTree(preorder, root_index + dis + 1, p_end, inorder, i_start + dis + 1, i_end);
         return root;
     }
 };
