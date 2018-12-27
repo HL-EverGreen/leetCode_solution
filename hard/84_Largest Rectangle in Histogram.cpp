cd618bb0d7c3fbca8fd09d3e36e72d93f642ce48
HL-EverGreen <lhr449307680@gmail.com>
2018-12-27 15:05:04 -0500

    array_stack
    array_stack

--- hard/84_Largest Rectangle in Histogram.cpp
+++ hard/84_Largest Rectangle in Histogram.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) { //stack_large_rectangle_in_histogram
-        int h_size=heights.size(), res=0, temp, start_idx;
-        stack<int> sta;
-        if(h_size==0) return 0;
+    int largestRectangleArea(vector<int>& heights) {
+        // array & stack
+        // time complexity: O(n)
+        // 8ms, beats 98.43%
+        
+        // when current heights[i] <= heights[index.top()], calculate new possible rectangle
+        int res = 0;
+        stack<int> index;
         heights.push_back(0);
-        for(int i=0;i<=h_size;i++){
-            while(sta.size()>0 && heights[sta.top()]>=heights[i]){
-                temp=heights[sta.top()];
-                sta.pop();
-                start_idx=sta.empty()? -1 : sta.top();
-                res=max(res,temp*(i-start_idx-1));
+        
+        for(int i = 0; i < heights.size(); i++) {
+            while(index.size() > 0 && heights[i] <= heights[index.top()]) {
+                int h = heights[index.top()];
+                index.pop();
+                
+                int left_idx = (index.empty() ? -1 : index.top());
+                res = max(res, h * (i - left_idx - 1));
             }
-            sta.push(i);
+            index.push(i);
         }
         return res;
     }
