ec6127d912660aa4c44125a7dbc89ae65fc963e7
HL-EverGreen <lhr449307680@gmail.com>
2018-12-26 22:27:12 -0500

    array
    array

--- medium/74_Search a 2D Matrix.cpp
+++ medium/74_Search a 2D Matrix.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {//matrix
-        int row=matrix.size(), col=row?matrix[0].size():0;
-        if(col==0) return false;
-        for(int i=0;i<row;i++){
-            if(matrix[i][col-1]<target) continue;
-            for(int j=col-1;j>=0;j--)
-                if(matrix[i][j]==target) return true;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // array
+        // time complexity: O(n)
+        // 8ms, beats 97.52%
+        
+        // search from right top corner, either go down or go left 
+        int row = matrix.size(), col = (row == 0 ? 0 : matrix[0].size());
+        if(col == 0) { return false; }
+        
+        int i = 0, j = col - 1;
+        while(i < row && j >= 0) {
+            if(matrix[i][j] == target) { 
+                return true; 
+            } else if(matrix[i][j] > target) {  // too large, search left
+                j--;
+            } else if(matrix[i][j] < target) {  // too small, search down
+                i++;
+            }
         }
         return false;
     }
