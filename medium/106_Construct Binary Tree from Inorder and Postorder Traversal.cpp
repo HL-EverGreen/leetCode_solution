80cd351503cc22b804c26ba9adb5412deb467acb
HL-EverGreen <lhr449307680@gmail.com>
2018-12-27 16:45:15 -0500

    array_binary_tree
    array_binary_tree

--- medium/106_Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ medium/106_Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -10,19 +10,29 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        return buildTree(inorder,0,inorder.size(),postorder,0,postorder.size());
+        // array & binary tree
+        // time complexity: O(n)
+        // 16ms, beats 48.18%
+
+        // method 1: using index
+        // method 2 is much better!!
+        return buildTree(inorder, 0, inorder.size(), postorder, 0, postorder.size());
     }
     
-    TreeNode* buildTree(vector<int>& inorder, int istart, int iend, vector<int>& postorder, int pstart, int pend){
-        if(istart>=iend || pstart>=pend) return nullptr;
-        auto mid=find(inorder.begin()+istart, inorder.begin()+iend, postorder[pend-1]);
-        int dis=inorder.begin()+iend-mid-1;
-        TreeNode* root=new TreeNode(postorder[pend-1]);
-        root->left=buildTree(inorder,istart,iend-dis-1,postorder,pstart,pend-dis-1);
-        root->right=buildTree(inorder,iend-dis,iend,postorder,pend-dis-1,pend-1);
+    TreeNode* buildTree(vector<int>& inorder, int i_start, int i_end, vector<int>& postorder, int p_start, int p_end) {
+        if(p_start >= p_end || i_start >= i_end) { return NULL; }
+        
+        int root_val = postorder[p_end - 1];
+        auto pos = find(inorder.begin(), inorder.begin() + i_end, root_val);
+        int dis = inorder.begin() + i_end - pos - 1;
+        
+        TreeNode* root = new TreeNode(root_val);
+        root->left = buildTree(inorder, i_start, i_end - dis - 1, postorder, p_start, p_end - dis - 1);
+        root->right = buildTree(inorder, i_end - dis, i_end, postorder, p_end - dis - 1, p_end - 1);
         return root;
     }
     
+    // method 2: using iterator
     //directly using iterator could be faster
     /*
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder)
@@ -45,6 +55,7 @@ public:
     }
     */
     
+    // method 3: using stack
     //using stack could even be faster
     /*
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
