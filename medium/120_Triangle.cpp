f786061c5c14e96ae86502283e8efe415792ea8d
HL-EverGreen <lhr449307680@gmail.com>
2018-12-27 20:35:24 -0500

    array
    array

--- medium/120_Triangle.cpp
+++ medium/120_Triangle.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {//dp
+    int minimumTotal(vector<vector<int>>& triangle) {
+        // array
+        // time complexity: O(n^2), space complexity: O(n)
+        // 4ms, beats 99.39%
+        
+        // method 1, do it in place
+        // update in place, from down to top, line by line
+        // or we can use new O(n) space and update values in that new O(n) space
+        int row = triangle.size(), min_sum = 0;
+        for(int i = row - 2; i >= 0; i--) {
+            for(int j = 0; j <= i; j++) {
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            }
+        }
+
+        // method 2, use new O(n) space
         //bottom to up
+        /*
         int row=triangle.size();
         if(row==0) return 0;
         vector<int> shortestPath=triangle[row-1];
@@ -9,6 +25,7 @@ public:
             for(int index_col=0;index_col<triangle[index_row].size();index_col++)
                 shortestPath[index_col]=min(shortestPath[index_col],shortestPath[index_col+1])+triangle[index_row][index_col];
         return shortestPath[0];
+        */
         
         //up to bottom, should find the least element in the last row, time consuming
         /*
