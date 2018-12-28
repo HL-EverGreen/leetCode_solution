2f68ee8d743bbaf933484e3f857037a7f060a9a4
HL-EverGreen <lhr449307680@gmail.com>
2018-12-27 21:09:29 -0500

    array
    array

--- medium/152_Maximum Product Subarray.cpp
+++ medium/152_Maximum Product Subarray.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) { //subarray
-        int frontProduct=1, backProduct=1, n_size=nums.size(), res=INT_MIN;
-        for(int i=0;i<n_size;i++){ //calculate product from two directions
-            frontProduct*=nums[i];
-            backProduct*=nums[n_size-1-i];
-            res=max(res,max(frontProduct, backProduct));
-            frontProduct=frontProduct==0?1:frontProduct; //eliminate 0's influence
-            backProduct=backProduct==0?1:backProduct;
+    int maxProduct(vector<int>& nums) {
+        // array
+        // time complexity: O(n), space complexity: O(1)
+        // 4ms, beats 99.26%
+        
+        // since all elements are integer (no fragmentation), so we can calculate products from two directions, be sure to eliminate influence of 0
+        int front_product = 1, back_product = 1, n_size = nums.size(), res = INT_MIN;
+        
+        for(int i = 0; i < n_size; i++) {
+            front_product *= nums[i];
+            back_product *= nums[n_size - i - 1];
+            res = max(res, max(front_product, back_product));
+            front_product = (front_product == 0 ? 1 : front_product);
+            back_product = (back_product == 0 ? 1 : back_product);
         }
         return res;
     }
