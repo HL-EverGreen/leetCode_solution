e1edc957f52a4ecd7d70914e3f667bea78937cf2
HL-EverGreen <lhr449307680@gmail.com>
2018-12-27 20:59:43 -0500

    array_dp
    array_dp

--- hard/123_Best Time to Buy and Sell Stock III.cpp
+++ hard/123_Best Time to Buy and Sell Stock III.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) { //dp
-        int states[2][4]={INT_MIN,0,INT_MIN,0};
-        int day=prices.size(), cur=0, next=1;
-        for(int i=0;i<day;i++){
-            states[next][0]=max(states[cur][0],-prices[i]);               //1 buy
-            states[next][1]=max(states[cur][1],states[cur][0]+prices[i]); //1 buy 1 sell
-            states[next][2]=max(states[cur][2],states[cur][1]-prices[i]); //2 buy 1 sell
-            states[next][3]=max(states[cur][3],states[cur][2]+prices[i]); //2 buy 2 sell
-            swap(cur,next);
+    int maxProfit(vector<int>& prices) {
+        // array & DP
+        // time complexity: O(n), space complexity: 0(1)
+        // 4ms, beats 99.70%
+        
+        // use four states to store current optimal solution and update it iteratively
+        int state[2][4] = {INT_MIN, 0, INT_MIN, 0};
+        int day = prices.size(), cur = 0, next = 1;
+        
+        for(int i = 0; i < day; i++) {
+            state[next][0] = max(state[cur][0], -prices[i]);                // 1 buy
+            state[next][1] = max(state[cur][1], state[cur][0] + prices[i]); // 1 buy & 1 sell
+            state[next][2] = max(state[cur][2], state[cur][1] - prices[i]); // 2 buy & 1 sell
+            state[next][3] = max(state[cur][3], state[cur][2] + prices[i]); // 2 buy & 2 sell
+            swap(next, cur);
         }
-        return max(states[cur][1],states[cur][3]);
+        return max(state[cur][1], state[cur][3]);
     }
 };
