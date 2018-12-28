deb1e912308f1a6663e0e986cee257a8d7630303
HL-EverGreen <lhr449307680@gmail.com>
2018-12-28 11:56:13 -0500

    array
    array

--- easy/189_Rotate Array.cpp
+++ easy/189_Rotate Array.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {  //move_array_k_times
-        int num_size=nums.size(), tmp=0, prev=0, replaced_count=0;
-        k%=num_size;
+    void rotate(vector<int>& nums, int k) {
+        // array
+        // time complexity: 0(n), space complexity: O(1)
+        // 16ms, beats 63.6%
         
-        for(int start=0;replaced_count<num_size;start++){ //swap element with k steps one time instead of 1 step
-            prev=nums[start];
-            int next_pos=(start+k)%num_size;
-            int stop=next_pos;
-            do{
-                tmp=nums[next_pos];
-                nums[next_pos]=prev;
-                prev=tmp;
-                next_pos=(next_pos+k)%num_size;
-                replaced_count++;
-            }while(next_pos!=stop);
-        }
+        // +k step replace until replace count >= size
+        int size = nums.size(), count = 0;
+        k %= size;
         
-        //insert operation consumes too much time
-        /*
-        k=k%nums.size();
-        for(int i=0;i<k;i++){
-            int tmp=nums.back();
-            nums.pop_back();
-            nums.insert(nums.begin(),tmp);
+        for(int i = 0; i < k && count < size; i++) {
+            int start = i, temp = nums[start];
+            while((start + size - k) % size != i) {
+                nums[start] = nums[(start + size - k) % size];
+                start = (start + size - k) % size;
+                count++;
+            }
+            nums[start] = temp;
+            count++;
         }
-        */
     }
 };
