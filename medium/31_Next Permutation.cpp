86a79219166a9b54640ab98e36359a8b48452418
HL-EverGreen <lhr449307680@gmail.com>
2018-12-28 23:37:39 -0500

    array
    array

--- medium/31_Next Permutation.cpp
+++ medium/31_Next Permutation.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {//next_permutation
-        int i=nums.size()-1, k=i;
-        //从后往前找到第一个小于后面元素的元素
-        //将该元素后所有元素反向
-        //从反向后该元素的位置向后找，找到第一个比该元素大的值并将其与该元素交换（若i==0则省略该步骤，即反向后已经是最小排列）
-        while(i>0 && nums[i-1]>=nums[i]) i--;
-        for(int j=i;j<k;j++,k--) swap(nums[j],nums[k]);
-        if(i>0){
-            k=i--; //k=i,i--;
-            while(nums[k]<=nums[i]) k++;
-            swap(nums[i],nums[k]);
+    void nextPermutation(vector<int>& nums) {
+        // array
+        // time complexity: O(n), space complexity: O(1)
+        // 8ms, beats 99.53%
+        
+        // 从后往前找到第一个小于后面元素的元素
+        // 将该元素后所有元素反向
+        // 从反向后该元素的位置向后找，找到第一个比该元素大的值并将其与该元素交换（若i==0则省略该步骤，即反向后已经是最小排列）
+        int i = nums.size() - 1;
+        while(i >= 1 && nums[i - 1] >= nums[i]) { i--; }
+        for(int j = i, k = nums.size() - 1; j < k;) { swap(nums[j++], nums[k--]); }
+        if(i > 0) {
+            int j = i--;
+            while(nums[j] <= nums[i]) { j++; }
+            swap(nums[j], nums[i]);
         }
     }
 };
