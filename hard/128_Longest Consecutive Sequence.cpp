f813fb51752ab8ec56d73f9fd1e29f3a38819f8a
HL-EverGreen <lhr449307680@gmail.com>
2018-12-29 14:20:45 -0500

    array_hash_table
    array_hash_table

--- hard/128_Longest Consecutive Sequence.cpp
+++ hard/128_Longest Consecutive Sequence.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        //use unordered_map, O(n)
-        /*
-        unordered_map<int,int> mp;
-        int res=0;
-        for(auto num:nums){
-            if(mp[num]) continue;
-            res=max(res,mp[num]=mp[num-mp[num-1]]=mp[num+mp[num+1]]=mp[num-1]+mp[num+1]+1);
-        }
-        return res;
-        */
+        // array & hash table
+        // time complexity: O(n), space complexity: O(n)
+        // 4ms, beats 100%
         
-        //use unordered_set, O(n)
-        unordered_set<int> tmp(nums.begin(),nums.end());
-        int prev,next,res=0;
-        for(auto num:nums){
-            if(tmp.find(num)==tmp.end()) continue;
-            //tmp.erase(num); //skip this step can save plenty of running time(as erase this num is unnecessary)
-            prev=num-1;
-            next=num+1;
-            while(tmp.find(prev)!=tmp.end()) tmp.erase(prev--);
-            while(tmp.find(next)!=tmp.end()) tmp.erase(next++);
-            res=max(res,(next-prev-1));
+        // method 1:
+        // using unordered_map, and mp[num] store its length longest consecuive array containing itself
+        unordered_map<int, int> mp;
+        int res = 0;
+        for(auto num : nums) {
+            if(mp[num]) { continue; }
+            res = max(res, mp[num] = mp[num + mp[num + 1]] = mp[num - mp[num - 1]] = mp[num + 1] + mp[num - 1] + 1);
         }
         return res;
         
+        // method 2:
+        // delete consecutive numbers in hash table in each round
+        // unordered_set<int> number(nums.begin(), nums.end());
+        // int prev, next, res = 0;
+        // for(auto num : nums) {
+        //     if(number.find(num) != number.end()) {
+        //         prev = num - 1; next = num + 1;
+        //         while(number.find(prev) != number.end()) { number.erase(prev--); }  // unordered_set erase time complexity: average O(1), worst O(n)
+        //         while(number.find(next) != number.end()) { number.erase(next++); }
+        //         res = max(res, next - prev - 1);
+        //     }
+        // }
+        // return res;
     }
 };
