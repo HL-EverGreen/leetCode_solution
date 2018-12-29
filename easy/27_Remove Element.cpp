ffa71c87f035e61f96d85100b46e47cca7570cad
HL-EverGreen <lhr449307680@gmail.com>
2018-12-28 22:48:25 -0500

    array
    array

--- easy/27_Remove Element.cpp
+++ easy/27_Remove Element.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==val){
-                nums.erase(nums.begin()+i,nums.begin()+i+1);
-                i--;
+    	// array
+        // time complexity: O(n), space complexity: O(1)
+        // 4ms, beats 98.88%
+        
+        // method 1
+        // two pointers
+        int start = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != val) {
+                nums[start++] = nums[i];
             }
         }
-        return nums.size();
+        return start;
+
+        // method 2
+        // using erase
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==val){
+        //         nums.erase(nums.begin()+i,nums.begin()+i+1);
+        //         i--;
+        //     }
+        // }
+        // return nums.size();
     }
 };
