f47c6bd4271612a1c2a3cda71dedda0734f39109
HL-EverGreen <lhr449307680@gmail.com>
2018-12-28 23:19:29 -0500

    array
    array

--- hard/85_Maximal Rectangle.cpp
+++ hard/85_Maximal Rectangle.cpp
@@ -1,56 +1,41 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>>& matrix) { // find max rectangle in Histogram
-        //main idea is to divide the matrix into each lines and transform into histogram
-        //so the question transform to find max rectangle in histogram
-        if(matrix.empty()) return 0;
-        int maxRec = 0;
-        vector<int> height(matrix[0].size(), 0);
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        // array
+        // time complexity: O(n^2), space complexity: O(n)
+        // 20ms, beats 46.74%
         
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[0].size(); j++){
-                if(matrix[i][j] == '0') height[j] = 0;
-                else height[j]++;
+        // main idea is to divide the matrix into each lines and transform into histogram
+        // so the question transform to find max rectangle in histogram
+        int row = matrix.size(), col = (row == 0 ? 0 : matrix[0].size()), res = 0;
+        if(col == 0) { return 0; }
+        vector<int> height(col, 0);
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                if(matrix[i][j] == '0') {
+                    height[j] = 0;
+                } else {
+                    height[j]++;
+                }
             }
-            maxRec = max(maxRec, largestRectangleArea(height));
-        }
-        return maxRec;
-    }
-
-    int largestRectangleArea(vector<int> &height) { //use LC84 solution
-        //a more easy to understand version
-        int h_size=height.size(), res=0, temp, start_idx;
-        stack<int> sta;
-        if(h_size==0) return 0;
-        height.push_back(0);
-        for(int i=0;i<=h_size;i++){
-            while(sta.size()>0 && height[sta.top()]>=height[i]){
-                temp=height[sta.top()];
-                sta.pop();
-                start_idx=sta.empty()? -1 : sta.top();
-                res=max(res,temp*(i-start_idx-1));
-            }
-            sta.push(i);
+            res = max(res, largestRectangle(height));
         }
         return res;
-
-        //not easy to understand
-        /*
-        stack<int> s;
+    }
+    
+    int largestRectangle(vector<int>& height) {
+        int res = 0;
+        stack<int> index;
         height.push_back(0);
-        int maxSize = 0;
-        for(int i = 0; i < height.size(); i++){
-            if(s.empty() || height[i] >= height[s.top()]){
-                s.push(i);
+        for(int i = 0; i < height.size(); i++) {
+            while(!index.empty() && height[i] < height[index.top()]) {
+                int h = height[index.top()];
+                index.pop();
+                int start_idx = index.empty() ? -1 : index.top();
+                res = max(res, h * (i - start_idx - 1));
             }
-            else{
-                int temp = height[s.top()];
-                s.pop();
-                maxSize = max(maxSize, temp*(s.empty()?i:i-1-s.top()));
-                i--;
-            }
+            index.push(i);
         }
-        return maxSize;
-        */
+        return res;
     }
 };
