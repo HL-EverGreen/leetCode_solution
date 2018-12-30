01737ebd5c8168b7f6f3d52fb302852c4779ded4
HL-EverGreen <lhr449307680@gmail.com>
2018-12-30 00:00:42 -0500

    array
    array

--- easy/121_Best Time to Buy and Sell Stock.cpp
+++ easy/121_Best Time to Buy and Sell Stock.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit=0;
-        if(prices.size()<2) return profit;
-        int low=prices[0];
-        int high;
+        // array
+        // time complexity: O(n), space complexity: O(1)
+        // 4ms, beats 99.51%
         
-        for(int i=1;i<prices.size();i++){
-            if(prices[i]<low){
-                low=prices[i];
-            }
-            else{
-                high=prices[i];
-                profit=max(profit,high-low);
-            }
+        int day = prices.size(), max_profit = 0, low_price = INT_MAX;
+        if(day < 2) { return max_profit; }
+        
+        for(int i = 0; i < day; i++) {
+            low_price = min(low_price, prices[i]);
+            max_profit = max(max_profit, prices[i] - low_price);
         }
-        return profit;
-        
+        return max_profit;
     }
 };
