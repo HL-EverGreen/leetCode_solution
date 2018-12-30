0d757b05a7da58b17d8fbfe9ce4ec270755c7eb3
HL-EverGreen <lhr449307680@gmail.com>
2018-12-30 17:25:03 -0500

    array
    array

--- easy/268_Missing Number.cpp
+++ easy/268_Missing Number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        vector<int> res(nums.size()+1,1);
-        for(int i=0;i<nums.size();i++){
-            res[nums[i]]--;
+        // array
+        // time complexity: O(n), space complexity: O(1)
+        // 16ms, beats 89.79%
+        
+        // XOR each elements and i from 0 to nums.size()
+        // so the res would be the missing number
+
+        int i = 0, res = nums.size(); // notice the initial of res would be nums.size() !!
+        for(auto num : nums) {
+            res ^= num;
+            res ^= i++;
         }
-        for(int i=0;i<=nums.size();i++){
-            if(res[i]==1) return i;
-        }
+        return res;
     }
 };
