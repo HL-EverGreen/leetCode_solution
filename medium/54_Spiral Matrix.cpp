35edbdd0eb617b70fd1b0670429c2a54e4a33666
HL-EverGreen <lhr449307680@gmail.com>
2018-12-29 23:30:58 -0500

    array
    array

--- medium/54_Spiral Matrix.cpp
+++ medium/54_Spiral Matrix.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) { //matrix
-        int row_end=matrix.size(), col_end=row_end?matrix[0].size():0, row_start=0, col_start=0;
-        if(col_end==0) return vector<int>();
-        int i=0,j=0;
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        // array
+        // time complexity: O(n^2), space complexity: O(n^2)
+        // 0ms, beats 100%
+        
+        // since matrix's length may not equals to width, we have to add break condition in loop
+        int row = matrix.size(), col = (row == 0 ? 0 : matrix[0].size());
+        int row_start = 0, row_end = row - 1, col_start = 0, col_end = col - 1;
         vector<int> res;
-        while(row_end>row_start && col_end>col_start){
-            for(int j=col_start;j<col_end;j++) res.push_back(matrix[row_start][j]);
-            if(++row_start==row_end) break;
-            for(int i=row_start;i<row_end;i++) res.push_back(matrix[i][col_end-1]);
-            if(--col_end==col_start) break;
-            for(int j=col_end-1;j>=col_start;j--) res.push_back(matrix[row_end-1][j]);
-            if(--row_end==row_start) break;
-            for(int i=row_end-1;i>=row_start;i--) res.push_back(matrix[i][col_start]);
-            if(++col_start==col_end) break;
+        
+        while(row_start <= row_end && col_start <= col_end) {
+            for(int i = col_start; i <= col_end; i++) {     // upper row, from left to right
+                res.push_back(matrix[row_start][i]);
+            }
+            if(++row_start == row_end + 1) { break; }
+            
+            for(int i = row_start; i <= row_end; i++) {     // right col, from top to down
+                res.push_back(matrix[i][col_end]);
+            }
+            if(--col_end == col_start - 1) { break; }
+            
+            for(int i = col_end; i >= col_start; i--) {     // lower row, from right to left
+                res.push_back(matrix[row_end][i]);
+            }
+            if(--row_end == row_start - 1) { break; }
+            
+            for(int i = row_end; i >= row_start; i--) {     // left col, from down to top
+                res.push_back(matrix[i][col_start]);
+            }
+            if(++col_start == col_end + 1) { break; }
         }
         return res;
     }
