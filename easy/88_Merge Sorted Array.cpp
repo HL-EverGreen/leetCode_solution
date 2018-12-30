2475e7593a01f690f8de3e0674d69cc587b1ca81
HL-EverGreen <lhr449307680@gmail.com>
2018-12-30 17:19:25 -0500

    array
    array

--- easy/88_Merge Sorted Array.cpp
+++ easy/88_Merge Sorted Array.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {//merge two sorted array
-        int index_n1=m-1, index_n2=n-1, tog=m+n-1;
-        while(index_n2>=0){ //add from end
-            nums1[tog--]=index_n1>=0 && nums1[index_n1]>nums2[index_n2]?nums1[index_n1--]:nums2[index_n2--];
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        // array
+        // time complexity: O(n), space complexity: O(1)
+        // 4ms, beats 99.35%
+        
+        // compare from end to start, and add elements from nums2 into nums1
+        int index1 = m - 1, index2 = n - 1, target = m + n - 1;
+        while(index2 >= 0) {
+            nums1[target--] = (index1 >= 0 && nums1[index1] > nums2[index2] ? nums1[index1--] : nums2[index2--]);
         }
     }
 };
