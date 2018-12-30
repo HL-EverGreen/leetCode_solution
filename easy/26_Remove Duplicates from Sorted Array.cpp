111fd34a6f51bcad220ef3b09e0b6bad0cc49ade
HL-EverGreen <lhr449307680@gmail.com>
2018-12-30 17:09:31 -0500

    array
    array

--- easy/26_Remove Duplicates from Sorted Array.cpp
+++ easy/26_Remove Duplicates from Sorted Array.cpp
@@ -1,11 +1,18 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {//two indexs
-        int i=0;
-        for(auto num:nums){
-            if(i<1 || num>nums[i-1]) //only element which differ from(bigger than) the former one could be placed in the array
-                nums[i++]=num;
+    int removeDuplicates(vector<int>& nums) {
+        // array
+        // time complexity: O(n), space complexity: O(1)
+        // 16ms, beats 99.35%
+        
+        // kind of two pointers
+        // only element which differ from (bigger than) the former one could be placed in the array
+        int start = 0;
+        for(auto num : nums) {
+            if(start < 1 || num > nums[start - 1]) {
+                nums[start++] = num;
+            }
         }
-        return i;
+        return start;
     }
 };
