7697db0369d2b4a4d8a0fa0a4e613a466e024793
HL-EverGreen <lhr449307680@gmail.com>
2018-12-30 18:45:14 -0500

    array
    array

--- medium/238_Product of Array Except Self.cpp
+++ medium/238_Product of Array Except Self.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int p=1;
-        vector<int> res;
-        if(nums.size()>0){
-            res.push_back(1);
-            for(int i=0;i<nums.size()-1;i++){ //product from left
-                res.push_back(res[i]*nums[i]);
-            }
-            for(int i=nums.size()-1;i>=0;i--){ //product from right
-                res[i]*=p;
-                p*=nums[i];
-            }    
+        // array
+        // time complexity: O(n), space complexity: O(n)
+        // 32ms, beats 99.25%
+        
+        // main idea: 
+        // First traverse: do product from left to right
+        // Second traverse: do product from right to left
+        int begin_prod = 1, back_prod = 1;
+        vector<int> res(nums.size(), 1);
+        
+        for(int i = 0; i < nums.size(); i++) {
+            res[i] *= begin_prod;
+            begin_prod *= nums[i];
         }
+        
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            res[i] *= back_prod;
+            back_prod *= nums[i];
+        }
+        
         return res;
     }
 };
