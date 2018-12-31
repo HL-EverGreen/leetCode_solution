f220d130948c031592c302ee8484373976a22242
HL-EverGreen <lhr449307680@gmail.com>
2018-12-30 21:14:32 -0500

    array_DP
    array_DP

--- medium/62_Unique Paths.cpp
+++ medium/62_Unique Paths.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {//math method
+        // array & DP
+        // time complexity: O(mn), space complexity: O(mn)
+        // 0ms, beats 100% 
+        
+        // general case: dp[i][j] = dp[i + 1][j] + dp[i][j + 1]
+        if(m <= 0 || n <= 0) { return 0; }
+        int path_num[m][n];
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                if(i == m - 1 && j == n - 1) {
+                    path_num[i][j] = 1;
+                } else if(i == m - 1) {
+                    path_num[i][j] = path_num[i][j + 1];
+                } else if(j == n - 1) {
+                    path_num[i][j] = path_num[i + 1][j];
+                } else{
+                    path_num[i][j] = path_num[i + 1][j] + path_num[i][j + 1];
+                }
+            }
+        }
+        return path_num[0][0];
+
+
+        // method 2:
+        // mathmatical way
+        /*
         if(m<=0 || n<=0) return 0;
         if(m==1 || n==1) return 1;
         double res=1;
@@ -9,22 +35,6 @@ public:
             res=res/(i+1);
         }
         return int(res);
-        
-        //or generally, recursive
-        /*
-         vector<vector<int>> cache(m, vector<int>(n, 0));
-        
-        cache[m-1][n-1] = 1;
-        for(int i=m-1; i>=0; --i) {
-            for(int j=n-1; j>=0; --j) {
-                if(i+1 < m)
-                    cache[i][j] += cache[i+1][j];
-                if(j+1 < n)
-                    cache[i][j] += cache[i][j+1];
-            }
-        }
-        
-        return cache[0][0];
         */
     }
-};
+}; 
