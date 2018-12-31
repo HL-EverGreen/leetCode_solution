97e8641dcfd8cf8375803dc73ec381d4995ab075
HL-EverGreen <lhr449307680@gmail.com>
2018-12-30 20:39:20 -0500

    array
    array

--- easy/118_Pascal's Triangle.cpp
+++ easy/118_Pascal's Triangle.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res;
-        for(int i=0;i<numRows;i++){
-            res.push_back(vector<int>());
-            res[i].push_back(1);
-            for(int j=1;j<i;j++)
-                res[i].push_back(res[i-1][j-1]+res[i-1][j]);
-            if(i!=0)
-                res[i].push_back(1);
+        // array
+        // time complexity: O(n^2), space complexity: O(n^2)
+        // 0ms, beats 100%
+        
+        vector<vector<int>> res(numRows);
+        
+        for(int i = 0; i < numRows; i++) {
+            res[i].resize(i + 1);
+            res[i][0] = res[i][i] = 1;
+            
+            for(int j = 1; j < i; j++) {
+                res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+            }
         }
         return res;
     }
