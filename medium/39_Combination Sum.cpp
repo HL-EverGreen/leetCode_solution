9144bcca0c840227efbb1dc9e5bfeacbbb9c3861
HL-EverGreen <lhr449307680@gmail.com>
2018-12-30 20:49:39 -0500

    array_DFS
    array_DFS

--- medium/39_Combination Sum.cpp
+++ medium/39_Combination Sum.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        // array & DFS
+        // time complexity: O(n^2), space complexity: O(n^2)
+        // 8ms, beats 99.81%
+        
         vector<vector<int>> res;
-        vector<int> temp;
-        sort(candidates.begin(),candidates.end());
-        dfs(res,temp,candidates,0,0,target);
+        vector<int> sol;
+        
+        sort(candidates.begin(), candidates.end());
+        findCombination(res, sol, candidates, target, 0);
         return res;
     }
     
-    void dfs(vector<vector<int>>& res, vector<int>& temp, vector<int>& can, int start, int sum, int target){
-        if(sum==target){
-            res.push_back(temp);
+    // DFS part
+    void findCombination(vector<vector<int>>& res, vector<int>& sol, vector<int>& candidates, int target, int start) {
+        if(target == 0) {
+            res.push_back(sol);
+            return;
         }
-        for(int i=start;i<can.size();i++){
-            if(sum+can[i]>target) return;
-            temp.push_back(can[i]);
-            dfs(res,temp,can,i,sum+can[i],target);
-            temp.pop_back();
+        for(int i = start; i < candidates.size(); i++) {
+            if(candidates[i] > target) { return; }
+            sol.push_back(candidates[i]);
+            findCombination(res, sol, candidates, target - candidates[i], i);
+            sol.pop_back();
         }
     }
 };
