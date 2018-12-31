9d00b68a732f0fe0579348b7504fcf48fefe9e4f
HL-EverGreen <lhr449307680@gmail.com>
2018-12-30 21:26:33 -0500

    array_DP_DC
    array_DP_DC

--- easy/53_Maximum Subarray.cpp
+++ easy/53_Maximum Subarray.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {//dp
-        int sum=0,res=nums[0];
-        for(auto num:nums){
-            sum+=num;
-            res=max(res,sum);
-            sum=max(sum,0);
+    int maxSubArray(vector<int>& nums) {
+        // array & DP & DC
+        // time complexity: O(n), space complexity: O(1)
+        // 4ms, beats 100%
+        
+        // much like DP, opt[i] = max(opt[i - 1] + A[i], 0)
+        int res = INT_MIN, sum = 0;
+        for(auto num : nums) {
+            sum += num;
+            res = max(res, sum);
+            sum = max(sum, 0);
         }
         return res;
+        
+        // DC's time complexity would be O(nlogn)
+        // can refer to 6140 assignment 4 problem 3.
     }
 };
