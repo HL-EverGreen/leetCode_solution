10f687995c0bf7d07981ca72931ceec186f9e5cb
HL-EverGreen <lhr449307680@gmail.com>
2019-01-02 12:35:36 -0500

    array
    array

--- easy/219_Contains Duplicate II.cpp
+++ easy/219_Contains Duplicate II.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
-    bool containsNearbyDuplicate(vector<int>& nums, int k) {//unordered_set
-        //the main idea is to store the last k elments in set and compare the new val to each element in the set
-        //and update set when set.size()>k
-        if(k<=0) return false;
-        int n_size=nums.size();
-        if(k>=n_size) k=n_size-1;
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        // array
+        // time complexity: O(n), space complexity: O(n)
+        // 16ms, beats 97.25%
+        
+        // method 1
+        // use hash table to record value and its index
+        unordered_map<int, int> count;
+        for(int i = 0; i < nums.size(); i++) {
+            if(count.find(nums[i]) != count.end() && i - count[nums[i]] <= k) { return true; }
+            count[nums[i]] = i;
+        }
+        return false;
+        
+        
+        // method 2
+        // use set and update set continually
+        /*
+        if(k <= 0) { return false; }
+        int n_size = nums.size();
+        if(k >= n_size) { k = n_size-1; }
         unordered_set<int> mySet;
         
-        for(int i=0;i<n_size;i++){
-            if(i>k) mySet.erase(nums[i-k-1]);
-            if(mySet.find(nums[i])!=mySet.end()) return true;
+        for(int i = 0; i < n_size; i++) {
+            if(i > k) { mySet.erase(nums[i - k - 1]); }
+            if(mySet.find(nums[i]) != mySet.end()) { return true; }
             mySet.insert(nums[i]);
         }
         return false;
+        */
     }
 };
