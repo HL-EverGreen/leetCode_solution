6e00e03bab38c81a0e195a9757940f18c4f2c117
HL-EverGreen <lhr449307680@gmail.com>
2019-01-03 15:12:13 -0500

    array
    array

--- medium/289_Game of Life.cpp
+++ medium/289_Game of Life.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    void gameOfLife(vector<vector<int>>& board) {//bit operation
-        //use two bits to solve this problem in-place
-        int row=board.size(),col=row?board[0].size():0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                int liv_level=0;
-                for(int i_x=max(i-1,0);i_x<min(i+2,row);i_x++){     //use max(),min() to address the border problem
-                    for(int j_y=max(j-1,0);j_y<min(j+2,col);j_y++){
-                        liv_level+=board[i_x][j_y]&1;               //use the current state to calculate
+    void gameOfLife(vector<vector<int>>& board) {
+        // array
+        // time complexity: O(n^2), space complexity: O(1)
+        // 0ms, beats 100%
+        
+        // use two bits to solve this problem in-place
+        // 1st bit: current status
+        // 2nd bit: future status
+        int row = board.size(), col = (row == 0) ? 0 : board[0].size();
+        
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                int liv_level = 0;
+                
+                for(int i_x = max(i - 1, 0); i_x < min(i + 2, row); i_x++) {            // use max(),min() to address the border problem
+                    for(int i_y = max(j - 1, 0); i_y < min(j + 2, col); i_y++) {
+                        liv_level += board[i_x][i_y] & 1;                               
                     }
                 }
-                if(liv_level==3 || liv_level-board[i][j]==3)
-                    board[i][j] |=2;                                //use two bits instead of one to present the next and current state
+                
+                if(liv_level == 3 || liv_level - board[i][j] == 3) {
+                    board[i][j] |= 2;
+                }
             }
         }
         
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                board[i][j]>>=1;
+         for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
+                board[i][j] >>= 1;                                                      // update second bit to first bit (future state to current state)
             }
-        }
+         }
     }
 };
