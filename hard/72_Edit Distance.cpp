0a6beb14e034603a8cc7b08338310f970969b1f0
HL-EverGreen <lhr449307680@gmail.com>
2019-01-03 17:15:24 -0500

    DP
    DP

--- hard/72_Edit Distance.cpp
+++ hard/72_Edit Distance.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {//dp
+    int minDistance(string word1, string word2) {
+        // dynamic programming
+        // time complexity: O(mn), space complexity: O(mn)
+        // 8ms, beats 94.07%
+        
         //classic problem of DP
         //We define the state dp[i][j] to be the minimum number of operations to convert word1[0..i - 1] to word2[0..j - 1]
         //Consider word[i - 1] and word2[j - 1]. If they are euqal, then no more operation is needed and dp[i][j] = dp[i - 1][j - 1]
@@ -15,20 +19,25 @@ public:
         //dp[i][j] = dp[i - 1][j - 1], if word1[i - 1] = word2[j - 1];
         //dp[i][j] = min(dp[i - 1][j - 1] + 1, dp[i - 1][j] + 1, dp[i][j - 1] + 1), otherwise.
         
-        int m=word1.size(),n=word2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
-        for(int i=1;i<=m;i++) {
-            dp[i][0]=i;
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dis(m + 1, vector<int>(n + 1, 0));
+        
+        for(int i = 1; i <= m; i++) {
+            dis[i][0] = i;
         }
-        for(int j=1;j<=n;j++){
-            dp[0][j]=j;
+        for(int j = 1; j <= n; j++) {
+            dis[0][j] = j;
         }
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
-                else dp[i][j]=min(1+dp[i-1][j-1],min(1+dp[i-1][j],1+dp[i][j-1]));
+        
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(word1[i - 1] == word2[j - 1]) {
+                    dis[i][j] = dis[i - 1][j - 1];
+                } else{
+                    dis[i][j] = min(dis[i - 1][j - 1] + 1, min(dis[i - 1][j] + 1, dis[i][j - 1] + 1));
+                }
             }
         }
-        return dp[m][n];
+        return dis[m][n];
     }
 };
