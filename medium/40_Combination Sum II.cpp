0c6d6578f9a79e00091bda11248cd0465f653a35
HL-EverGreen <lhr449307680@gmail.com>
2019-01-03 12:11:28 -0500

    array_DFS
    array_DFS

--- medium/40_Combination Sum II.cpp
+++ medium/40_Combination Sum II.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {//dfs
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        // array & DFS
+        // time complexity: O(2^n), space complexity: O(2^n)
+        // 4ms, beats 100%
+        
         vector<vector<int>> res;
-        vector<int> tmp;
-        sort(candidates.begin(),candidates.end());
-        dfs(res,tmp,candidates,target,0,0);
+        vector<int> sol;
+        sort(candidates.begin(), candidates.end());
+        findCombination(res, sol, candidates, target, 0);
         return res;
     }
     
-    void dfs(vector<vector<int>>& res, vector<int>& tmp, vector<int>& candidates, int target, int sum, int start){
-        if(sum==target){
-            res.push_back(tmp);
+    // DFS part
+    void findCombination(vector<vector<int>>& res, vector<int>& sol, vector<int>& candidates, int target, int start) {
+        if(target == 0) { 
+            res.push_back(sol);
             return;
         }
-        
-        for(int i=start;i<candidates.size();i++){
-            while(i>0 && candidates[i]==candidates[i-1] && i>start) i++; //to ensure no duplicates in res
-            int cand=candidates[i];
-            if(sum+cand>target) return;
-            tmp.push_back(cand);
-            dfs(res,tmp,candidates,target,sum+cand,i+1);
-            tmp.pop_back();
+        for(int i = start; i < candidates.size(); i++) {
+            if(candidates[i] > target) { return; }
+            if(i == start || candidates[i] > candidates[i - 1]) {                       // ensure no duplicated solutions
+                sol.push_back(candidates[i]);
+                findCombination(res, sol, candidates, target - candidates[i], i + 1);
+                sol.pop_back();
+            }
         }
     }
 };
