440f7620897efeb42399c69499c83d2774881ce4
HL-EverGreen <lhr449307680@gmail.com>
2019-01-05 17:31:32 -0500

    DP
    DP

--- medium/300_Longest Increasing Subsequence.cpp
+++ medium/300_Longest Increasing Subsequence.cpp
@@ -1,32 +1,39 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        //Time complexity: O(nlogn)
-        //Using an timely updating sequence, when a smaller number come up, replace the corresponding old bigger number with it
+        // method 1
+        // time complexity: O(nlogn), space complexity: O(n)
+        // 4ms, beats 81.83%
+        // Using an timely updating sequence, when a smaller number come up, replace the corresponding old bigger number with it
+        
         vector<int> res;
-        for(auto num:nums){
-            auto it=lower_bound(res.begin(),res.end(),num);//use binary search to find the index of first number bigger than num
-            if(it==res.end()) res.push_back(num);
-            else *it=num;
+        for(auto num : nums) {
+            auto it = lower_bound(res.begin(), res.end(), num);         // lower_bound: find the least element >= specified element, O(logn) - use binary search to find
+                                                                        // upper_bound: find the least element > specified element, O(logn) - use binary search to find
+            if(it == res.end()) { res.push_back(num); }
+            else { *it = num; }
         }
         return res.size();
         
         
+        // method 2
+        // dynamic programming
+        // time complexity: O(n^2), space complexity: O(n)
         
-        //Time complexity: O(n^2)
-        //Using DP, the essensial of dp is to find the meaning of dp[i]
-        //Here, the dp[i] represents the length of the longest increasing sequence which ends with nums[i]
-        //So that when nums[j]>nums[i],dp[j]=max(dp[j],dp[i]+1)
+        // Using DP, the essensial of dp is to find the meaning of dp[i]
+        // Here, the dp[i] represents the length of the longest increasing sequence which ends with nums[i]
+        // So that when nums[j]>nums[i],dp[j]=max(dp[j],dp[i]+1)
         /*
-        if(nums.size()==0) return 0;
-        int size=nums.size(),res=1;
-        vector<int> dp(size,1);
-        for(int i=1;i<size;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j])
-                    dp[i]=max(dp[i],dp[j]+1);
+        int size = nums.size(), res = 1;
+        if(size < 1) { return 0; }
+        vector<int> max_len(size, 1);
+        for(int i = 1; i < size; i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    max_len[i] = max(max_len[i], max_len[j] + 1);
+                }
+                res = max(res, max_len[i]);
             }
-            res=max(res,dp[i]);
         }
         return res;
         */
