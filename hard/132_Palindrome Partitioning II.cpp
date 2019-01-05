6b06225d37e1b8892bc4c190f2c86fe92bf2953b
HL-EverGreen <lhr449307680@gmail.com>
2019-01-04 22:11:44 -0500

    DP
    DP

--- hard/132_Palindrome Partitioning II.cpp
+++ hard/132_Palindrome Partitioning II.cpp
@@ -1,30 +1,49 @@
 class Solution {
 public:
     int minCut(string s) {
-        //dp
-        //time complexity: O(n^2)
-        //isPalin[i][j] means whether string s[i]...s[j] is palindrome
-        //minCut[i] means minimum number of cut needed to divide string s[0]...s[i-1] into all palindrome words
+        // dynamic programming
         
-        int s_size = s.length();
-        if(s_size <= 1) return 0;
+        // method 1 (method 2 is faster)
+        // time complexity: O(n^2), space complexity: O(n^2)
+        // 8ms, beats 77.54%
         
-        bool isPalin[s_size][s_size];
-        fill_n(&isPalin[0][0], s_size * s_size, false);
+        // is_palin[i][j]: whether string s[i]...s[j] is palindrome
+        // min_cut[i]: minimum number of cut needed to divide string s[0]...s[i-1] into all palindrome words
         
-        int minCut[s_size + 1];
-        for(int idx = 0; idx <= s_size; idx++){
-            minCut[idx] = idx - 1;
-        }
+        int size = s.length();
+        if(size <= 1) { return 0; }
+        bool is_palin[size][size] = {false};
+        //fill_n(&is_palin[0][0], size * size, false);
+        int min_cut[size + 1];
+        for(int i = 0; i <= size; i++) { min_cut[i] = i - 1; }
         
-        for(int j = 1; j < s_size; j++){
-            for(int i = j; i >= 0; i--){
-                if(s[j] == s[i] && (j - i < 2 || isPalin[i + 1][j - 1])){
-                    isPalin[i][j] = true;
-                    minCut[j + 1] = min(minCut[j + 1], 1 + minCut[i]);
+        for(int i = 1; i < size; i++) {
+            for(int j = i; j >= 0; j--) {
+                if(s[i] == s[j] && (i - j < 2 || is_palin[j + 1][i - 1])) {
+                    is_palin[j][i] = true;
+                    min_cut[i + 1] = min(min_cut[i + 1], 1 + min_cut[j]);
                 }
             }
         }
-        return minCut[s_size];
+        return min_cut[size];
+        
+        
+        // method 2
+        // another DP solution only require O(n) space since it does not need is_palin table
+        // time complexity: O(n^2), space complexity: O(n)
+        // 4ms, beats 93.54%
+        /*
+        int n = s.size();
+        vector<int> cut(n+1, 0);  // number of cuts for the first k characters
+        for (int i = 0; i <= n; i++) { cut[i] = i-1; }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; i - j >= 0 && i + j < n && s[i - j] == s[i + j] ; j++) // odd length palindrome
+                cut[i + j + 1] = min(cut[i + j + 1], 1 + cut[i - j]);
+
+            for (int j = 1; i - j + 1 >= 0 && i + j < n && s[i - j + 1] == s[i + j]; j++) // even length palindrome
+                cut[i + j + 1] = min(cut[i + j + 1], 1 + cut[i - j + 1]);
+        }
+        return cut[n];
+        */
     }
 };
