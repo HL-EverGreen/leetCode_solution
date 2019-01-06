3d9078f4350936f624ab5c538e3774e5518c9fa0
HL-EverGreen <lhr449307680@gmail.com>
2019-01-05 20:19:13 -0500

    DP
    DP

--- medium/96_Unique Binary Search Trees.cpp
+++ medium/96_Unique Binary Search Trees.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int numTrees(int n) {//Dynamic programming
-        vector<int> res;
-        res.push_back(1);
-        res.push_back(1);
-        for(int i=2;i<=n;i++){
-            int temp=0;
-            for(int j=0;j<i;j++){
-                temp+=res[j]*res[i-j-1];
+    int numTrees(int n) {
+        // dynamic programming
+        // time complexity: O(n^2), space complexity: O(n)
+        // 0ms, beats 100%
+        
+        // number of nodes on left side of root: from 0 ~ i - 1
+        if(n < 1) { return 0; }
+        vector<int> uni_bst_num(n + 1, 0);
+        uni_bst_num[0] = uni_bst_num[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                uni_bst_num[i] += uni_bst_num[j] * uni_bst_num[i - j - 1];
             }
-            res.push_back(temp);
         }
-        return res[n];
+        return uni_bst_num[n];
     }
 };
