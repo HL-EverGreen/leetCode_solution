7387bd776359afe38a65d372405aa5cd4ee4711c
HL-EverGreen <lhr449307680@gmail.com>
2019-01-05 19:47:25 -0500

    DP
    DP

--- medium/221_Maximal Square.cpp
+++ medium/221_Maximal Square.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {//dp
-        //define dp[i][j] as the maximal size of the square that can be achieved at point (i, j)
-        int row=matrix.size(), col=row?matrix[0].size():0,res=0;
-        if(col==0) return 0;
-        vector<vector<int>> dp(row,vector<int>(col,0));
-        for(int i=0;i<row;i++){
-            dp[i][0]=matrix[i][0]-'0';
-            res=max(res,dp[i][0]);
+    int maximalSquare(vector<vector<char>>& matrix) {
+        // dynamic programming
+        // time complexity: O(n^2), space complexity: O(1)
+        // 20ms, beats 54.85%
+        
+        // define dp[i][j] as the maximal length of the square that can be achieved at point (i, j)
+        int row = matrix.size(), col = (row == 0 ? 0 : matrix[0].size()), res = 0;
+        if(col == 0) { return 0; }
+        
+        // initial
+        for(int i = 0; i < row; i++) {
+            if(matrix[i][0] == '1') { 
+                res = 1;
+                break;
+            }
         }
-        for(int j=1;j<col;j++) {
-            dp[0][j]=matrix[0][j]-'0';
-            res=max(res,dp[0][j]);
-        }
-        for(int i=1;i<row;i++){
-            for(int j=1;j<col;j++){
-                if(matrix[i][j]=='1') {
-                    dp[i][j]=min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
-                    res=max(res,dp[i][j]);
+        if(res == 0) {
+            for(int j = 0; j < col; j++) {
+                if(matrix[0][j] == '1') { 
+                    res = 1;
+                    break;
                 }
             }
         }
-        return res*res;
+        
+        // DP part
+        for(int i = 1; i < row; i++) {
+            for(int j = 1; j < col; j++) {
+                if(matrix[i][j] == '1') {
+                    matrix[i][j] = min(matrix[i - 1][j - 1], min(matrix[i - 1][j], matrix[i][j - 1])) + 1;
+                    res = max(res, matrix[i][j] - '0');
+                }
+            }
+        }
+        return res * res;
     }
 };
