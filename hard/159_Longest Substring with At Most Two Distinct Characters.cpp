6c6f60c6a9989e7826d3d99c783480ee263ca19c
HL-EverGreen <lhr449307680@gmail.com>
2019-01-14 15:39:04 -0500

    string_two pointers
    string_two pointers

--- hard/159_Longest Substring with At Most Two Distinct Characters.cpp
+++ hard/159_Longest Substring with At Most Two Distinct Characters.cpp
@@ -1,19 +1,47 @@
-int lengthOfLongestSubstringTwoDistinct(string s) {// two pointers
-    if(s.empty()) return 0;
-    int dict[256]; 
-    fill_n(dict,256,0);
-    int start = 0, len = 1, count = 0;
-    for(int i=0; i<s.length(); i++) {
-        dict[s[i]]++;
-        if(dict[s[i]] == 1) { // new char
-            count++;
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        // string & two pointers
+        // time complexity: O(n), space complexity: O(1)
+        
+        // method 1:
+        // use table
+        // 4ms, beats 100%
+        /*
+        int dict[256] = {0}, len = 0, start = 0, count = 0;
+        //fill_n(dict, 256, 0);
+        for(int i = 0; i < s.size(); i++) {
+            if(++dict[s[i]] == 1) { count++; }
             while(count > 2) {
-                dict[s[start]]--;
-                if(dict[s[start]] == 0) count--; 
+                if(--dict[s[start]] == 0) { count--; }
                 start++;
             }
+            len = max(len, i - start + 1);
         }
-        if(i-start+1 > len) len = i-start+1;
+        return len;
+        */
+        
+        // method 2:
+        // use two variables
+        // 4ms, beats 100%
+        int len = 0, cur = 0, lastOneCount = 0, num1 = s[0], num2 = s[0];
+        for(char ch : s) {
+            if(ch == num2) {
+                lastOneCount++;
+                cur++;
+            } else if(ch == num1) {
+                lastOneCount = 1;
+                cur++;
+                num1 = num2;
+                num2 = ch;
+            } else {
+                cur = lastOneCount + 1;
+                lastOneCount = 1;
+                num1 = num2;
+                num2 = ch;
+            }
+            len = max(len, cur);
+        }
+        return len;
     }
-    return len;
-}
+};
