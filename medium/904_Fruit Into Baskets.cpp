c0353cdaad75393343d5ba2c527c2129b8285194
HL-EverGreen <lhr449307680@gmail.com>
2019-01-14 11:48:37 -0500

    array_two pointers
    array_two pointers

--- medium/904_Fruit Into Baskets.cpp
+++ medium/904_Fruit Into Baskets.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    int totalFruit(vector<int>& tree) { //max subarray with two distinct characters
-        //two variables solution, much more faster
+    int totalFruit(vector<int>& tree) {
+        // two variable
+        // time complexity: O(n), space complexity: O(1)
+        // 132ms, beats 79.89%
+        
+        // num1: last second number appeared
+        // num2: last number appeared
+        // lastOneCount: last appeared number's count
+        // cur: current total fruits' count
         int cur = 0, lastOneCount = 0, res = 0, num1 = tree[0], num2 = tree[0];
-        for(int fruit : tree){
-            if(fruit == num2){
+        for(int fruit : tree) {
+            if(fruit == num2) {
                 lastOneCount++;
                 cur++;
-            }else if(fruit == num1){
+            } else if(fruit == num1) {
                 lastOneCount = 1;
                 cur++;
                 num1 = num2;
                 num2 = fruit;
-            }else{
+            } else {
                 cur = lastOneCount + 1;
                 lastOneCount = 1;
                 num1 = num2;
@@ -21,22 +28,22 @@ public:
             res = max(res, cur);
         }
         return res;
-
-        //LC 159 solution, find max subarray with two distinct characters
+        
+        // use hash table
+        // time complexity: O(n), space complexity: O(n)
+        // 168ms, beats 51.27%
         /*
-        int size=tree.size(), start=0, len=1, count=0;
-        unordered_map<int,int> basket;
-        for(int i=0;i<size;i++){
-            int tmp=++basket[tree[i]];
-            if(tmp==1) count++;
-            while(count>2){
-                tmp=--basket[tree[start]];
-                if(tmp==0) count--;
+        int count = 0, start = 0, res = 0;
+        unordered_map<int, int> fruit;
+        for(int i = 0; i < tree.size(); i++) {
+            if(++fruit[tree[i]] == 1) { count++; }
+            while(count > 2) {
+                if(--fruit[tree[start]] == 0) { count--; }
                 start++;
             }
-            len=max(i-start+1,len);
+            res = max(res, i - start + 1);
         }
-        return len;
+        return res;
         */
     }
 };
