bf962c1fd69d8c0fda9bae2650aae3cfdbf2447e
HL-EverGreen <lhr449307680@gmail.com>
2019-01-14 14:24:54 -0500

    string
    string

--- medium/681_Next Closest Time.cpp
+++ medium/681_Next Closest Time.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int scale[4]={600,60,10,1};
-    string nextClosestTime(string time) { //string
-        size_t pos=time.find(':');
-        int cur_time=stoi(time.substr(0,pos))*60+stoi(time.substr(pos+1));
-        string res="0000";
-        for(int i=1,digit=0;i<=1440 && digit<4;i++){
-            int nor_time=(cur_time+i)%1440;
-            for(digit=0;digit<4;digit++){
-                res[digit]='0'+nor_time/scale[digit];
-                nor_time%=scale[digit];
-                if(time.find(res[digit])==string::npos) break;
+    string nextClosestTime(string time) {
+        // string
+        // time complexity: O(1), space complexity: O(1)
+        // 0ms, beats 100%
+        
+        // check each possibilities
+        int unit[4] = {600, 60, 10, 1};
+        int pos = time.find(':'), cur = stoi(time.substr(0, pos)) * 60 + stoi(time.substr(pos + 1));
+        string next = "0000";
+        for(int i = 1, d = 0; i <= 1440 && d < 4; i++) {
+            int t = (cur + i) % 1440;
+            for(d = 0; d < 4; d++) {
+                next[d] = '0' + t / unit[d];
+                t %= unit[d];
+                if(time.find(next[d]) == string::npos) { break; }
             }
         }
-        return res.substr(0,2)+':'+res.substr(2,2);
+        return next.substr(0, 2) + ':' + next.substr(2);
     }
 };
