4aab5d6af7a6e1b9deabd1d4b83dcb20ed5004d2
HL-EverGreen <lhr449307680@gmail.com>
2019-01-13 22:25:00 -0500

    list_two pointers
    list_two pointers

--- medium/61_Rotate List.cpp
+++ medium/61_Rotate List.cpp
@@ -8,30 +8,31 @@
  */
 class Solution {
 public:
-    ListNode* rotateRight(ListNode* head, int k) { 
-        //list
-        //time complexity: O(n)
-
-        //if less than one node
-        if(!head || head -> next == NULL) return head;
+    ListNode* rotateRight(ListNode* head, int k) {
+        // list & two pointers
+        // time complexity: O(n), space complexity: O(1)
+        // 8ms, beats 99.71%
+        
+        // if less than one node
+        if(!head || head -> next == NULL) { return head; }
         int len = 1;
-        ListNode* temp = head;
-        while(temp -> next){
-            temp = temp -> next;
+        ListNode* cur = head;
+        while(cur -> next) {
             len++;
+            cur = cur -> next;
         }
         
-        //connect tail and head to construct a circle
-        temp -> next = head;
+        // connect tail and head to construct a circle
+        cur -> next = head;
         
-        //construct new list
-        if(k %= len){
-            for(int count = 0; count < len - k; count++){
-                temp = temp -> next;
+        // construct new list
+        if(k %= len) {
+            for(int i = 0; i < len - k; i++) {
+                cur = cur -> next;    
             }
         }
-        head = temp -> next;
-        temp -> next = NULL;
+        head = cur -> next;
+        cur -> next = NULL;
         return head;
     }
 };
