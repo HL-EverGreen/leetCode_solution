4005520db5c804a8ad3e55020db1af868215039a
HL-EverGreen <lhr449307680@gmail.com>
2019-01-14 21:38:36 -0500

    array
    array

--- hard/683_K Empty Slots.cpp
+++ hard/683_K Empty Slots.cpp
@@ -9,13 +9,13 @@ public:
         // so a solution satisfies: for every left < i < right, days[i] > days[left] && days[i] > days[right]
         vector<int> days(flowers.size());
         for(int i = 0; i < flowers.size(); i++) { days[flowers[i] - 1] = i + 1; }
-            
+
         int left = 0, right = k + 1, res = flowers.size();
         for(int i = 0; right < flowers.size(); i++) {
             if(days[i] < days[left] || days[i] <= days[right]) {
                 if(i == right) { res = min(res, max(days[left], days[right])); }        // if get a valid solution
                 left = i;
-                right = i + k + 1;
+                right = i + k + 1;                                                      // update new situation
             }
         }
         return res == flowers.size() ? -1 : res;
