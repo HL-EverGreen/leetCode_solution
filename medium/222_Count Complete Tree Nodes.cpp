dabed89b1b93130d56e4f8a6720c895fbc75d5bb
HL-EverGreen <lhr449307680@gmail.com>
2019-01-16 11:55:27 -0500

    binary_tree
    binary_tree

--- medium/222_Count Complete Tree Nodes.cpp
+++ medium/222_Count Complete Tree Nodes.cpp
@@ -8,22 +8,22 @@
  * };
  */
 class Solution {
-public:
-    int height(TreeNode* root){
-        if(!root) return -1;
-        return 1+height(root->left);
-    }
-    
+public:    
     int countNodes(TreeNode* root) { //complete tree property
-        if(!root) return 0;
-        int h=height(root), res=0;
-        while(h>=0){
-            if(height(root->right) == h-1){ //说明左子树满
-                res+= 1<<h;
-                root=root->right;
-            }
-            else{                           //说明右子树最后一层全空
-                res+=1<<(h-1);
+        // binary tree
+        // time complexity: O(log(n)^2), log(n) ~ tree's height, space complexity: O(1)
+        // 16ms, beats 99.34%
+        
+        // compare root->right's height with h - 1
+        // if == h - 1, then left subtree is full
+        // if != h - 1, then right subtree's last layer is empty
+        int h = height(root), res = 0;
+        while(h >= 0) {
+            if(height(root->right) == h - 1) {  // left subtree is full
+                res += 1<<h;
+                root = root->right;
+            } else {                            // right subtree's last layer is empty
+                res += 1<<(h - 1);
                 root = root->left;
             }
             h--;
@@ -40,5 +40,10 @@ public:
         if(cl==cr) return pow(2,cl)-1;
         return 1+countNodes(root->left)+countNodes(root->right);
         */
+    }
+
+    int height(TreeNode* root) {
+        if(!root) { return -1; }
+        return 1 + height(root->left);
     }
 };
