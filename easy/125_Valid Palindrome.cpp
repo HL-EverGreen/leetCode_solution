416adb166339a4e28bd336120aa546a312e3c7c3
HL-EverGreen <lhr449307680@gmail.com>
2019-01-17 10:32:00 -0500

    string
    string

--- easy/125_Valid Palindrome.cpp
+++ easy/125_Valid Palindrome.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
-    bool isPalindrome(string s) { //string
-        for(int i=0,j=s.size()-1;i<j;i++,j--){
-            while(!isalnum(s[i]) && i<j) i++;   //determine if is alphanumeric
-            while(!isalnum(s[j]) && i<j) j--;
-            if(toupper(s[i])!=toupper(s[j])) return false;   //change to uppercase first
+    bool isPalindrome(string s) {
+        // string
+        // time complexity: O(n), space complexity: O(1)
+        // 8ms, beats 98.46%
+        
+        for(int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            while(i < j && !isalnum(s[i])) i++;
+            while(i < j && !isalnum(s[j])) j--;
+            if(toupper(s[i]) != toupper(s[j])) return false;
         }
         return true;
     }
