9e75bc233a5c2abe022f7f657e453b53eacbf652
HL-EverGreen <lhr449307680@gmail.com>
2019-01-17 13:04:25 -0500

    string
    string

--- medium/165_Compare Version Numbers.cpp
+++ medium/165_Compare Version Numbers.cpp
@@ -2,31 +2,29 @@ class Solution {
 public:
     int compareVersion(string version1, string version2) {
         // string
-        // time complexity: O(n), n is the shorter string's length
+        // time complexity: O(n), space complexity: O(1)
+        // 0ms, beats 100%
         
-        int idx1 = 0, idx2 = 0;
+        // compute and compare each number between dots respectly
         int n1 = version1.length(), n2 = version2.length();
-        
-        while(idx1 < n1 || idx2 < n2) {
+        for(int i = 0, j = 0; i < n1 || j < n2; i++, j++) {
             int num1 = 0, num2 = 0;
             
             // compute current version number for VERSION1
-            while(idx1 < n1 && version1[idx1] != '.') {
-                num1 = 10* num1 + (version1[idx1] - '0');
-                idx1++;
+            while(i < n1 && version1[i] != '.') {
+                num1 = num1 * 10 + (version1[i] - '0');
+                i++;
             }
             
             // compute current version number for VERSION2
-            while(idx2 < n2 && version2[idx2] != '.') {
-                num2 = 10 * num2 + (version2[idx2] - '0');
-                idx2++;
+            while(j < n2 && version2[j] != '.') {
+                num2 = num2 * 10 + (version2[j] - '0');
+                j++;
             }
             
             // compare the version number
+            if(num1 < num2) return -1;
             if(num1 > num2) return 1;
-            else if(num1 < num2) return -1;
-            
-            idx1++; idx2++;
         }
         return 0;
     }
