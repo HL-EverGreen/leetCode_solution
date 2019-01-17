0e92c275ae5b030a75db46e78bef122a19b99a9d
HL-EverGreen <lhr449307680@gmail.com>
2019-01-17 11:21:57 -0500

    string_hash table
    string_hash table

--- medium/49_Group Anagrams.cpp
+++ medium/49_Group Anagrams.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {//sort_string_and_unordered_map
-        unordered_map<string,vector<string>> mp;
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // string & hash table
+        // time complexity: O(n^2logn), space complexity: O(n)
+        // 28ms, beats 96.42%
+        
+        // actually we can use special sort instead of sort (which takes nlogn time), code is listed below
+        unordered_map<string, vector<string>> group;
         vector<vector<string>> res;
-        //int i=0;
-        for(auto s:strs){
-            string tmp=s;
-            sort(s.begin(),s.end());
-            mp[s].push_back(tmp); //use tmp=s instead strs[i] to save running time
+        for(auto s: strs) {
+            string tmp = s;
+            sort(s.begin(), s.end());
+            group[s].push_back(tmp);
         }
-        for(auto m:mp){
-            res.push_back(m.second);
+        
+        for(auto g : group) {
+            res.push_back(g.second);
         }
+        
         return res;
     }
+    
+    // special sort function, takes O(n) time
+    /*
+    string strSort(string& s) {
+        int count[26] = {0}, n = s.length();
+        for (int i = 0; i < n; i++)
+            count[s[i] - 'a']++;
+        int p = 0;
+        string t(n, 'a');
+        for (int j = 0; j < 26; j++)
+            for (int i = 0; i < count[j]; i++)
+                t[p++] += j;
+        return t;
+    } 
+    */
 };
