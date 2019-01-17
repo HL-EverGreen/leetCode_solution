16e7c43b3cddc66e1e3212dac971f73a3b41dbbf
HL-EverGreen <lhr449307680@gmail.com>
2019-01-16 23:51:39 -0500

    string
    string

--- easy/67_Add Binary.cpp
+++ easy/67_Add Binary.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    string addBinary(string a, string b) {//bit operation
-        string res="";
-        int c=0,index_a=a.size()-1,index_b=b.size()-1;
-        while(index_a>=0 || index_b>=0 || c==1){
-            c+=index_a>=0?a[index_a--]-'0':0;
-            c+=index_b>=0?b[index_b--]-'0':0;
-            res=char(c % 2 + '0') + res;
-            c/=2;
+    string addBinary(string a, string b) {
+        // string
+        // time complexity: O(n), space complexity: O(1)
+        // 4ms, beats 99.53%
+        
+        // add from back to front, add up by a[i], b[j] and carry
+        int n1 = a.length() - 1, n2 = b.length() - 1, carry = 0;
+        string res ="";
+        while(n1 >= 0 || n2 >= 0 || carry) {
+            int cur = (n1 >=0 ? a[n1--] - '0' : 0) + (n2 >=0 ? b[n2--] - '0' : 0) + carry;
+            carry = cur / 2;
+            cur %= 2;
+            res = to_string(cur) + res;
         }
         return res;
     }
