fc771657ebf125841148a08af1a271c04b0994df
HL-EverGreen <lhr449307680@gmail.com>
2019-01-17 10:09:15 -0500

    string_DFS
    string_DFS

--- medium/93_Restore IP Addresses.cpp
+++ medium/93_Restore IP Addresses.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    vector<string> restoreIpAddresses(string s) {//dfs
+    vector<string> restoreIpAddresses(string s) {
+        // string & DFS
+        // 0ms, beats 100%
+        
+        // each round dfs from start to start + 3
         vector<string> res;
         string ip;
-        dfs(s,ip,res,0,0);
+        findIP(res, s, ip, 0, 0);
         return res;
     }
     
-    void dfs(string s, string ip, vector<string>& res, int start, int step){
-        if(start==s.size() && step==4){
-            ip.erase(ip.end()-1); //remove the last '.' from the last decimal number
+    void findIP(vector<string>& res, string s, string ip, int start, int step) {
+        if(start == s.length() && step == 4) {
+            ip.erase(ip.end() - 1);                     // remove the last '.' from the last decimal number
             res.push_back(ip);
             return;
         }
-        if(s.size()-start>(4-step)*3) return;
-        if(s.size()-start<(4-step)) return;
-        int num=0;
-        for(int i=start;i<start+3;i++){
-            num=num*10+(s[i]-'0');
-            if(num<=255){
-                ip+=s[i];
-                dfs(s,ip+'.',res,i+1,step+1);
+        
+        if(s.size() - start > (4 - step) * 3) return;
+        if(s.size() - start < 4 - step) return;
+        int num = 0;
+        for(int i = start; i < start + 3; i++) {
+            num = num * 10 + (s[i] - '0');
+            if(num <= 255) {
+                ip += s[i];
+                findIP(res, s, ip + '.', i + 1, step + 1);
             }
-            if(num==0) break;
+            if(num == 0) break;
         }
     }
 };
