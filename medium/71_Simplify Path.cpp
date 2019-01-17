eb6c130096aae73ae6819f098c3e2388dcdb0216
HL-EverGreen <lhr449307680@gmail.com>
2019-01-17 11:45:21 -0500

    string
    string

--- medium/71_Simplify Path.cpp
+++ medium/71_Simplify Path.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    string simplifyPath(string path) { //stringstream_getline
-        string res,tmp;
+    string simplifyPath(string path) {
+        // string
+        // time complexity: O(n), space complexity: O(n)
+        // 8ms, beats 100%
+        
+        string res, tmp;
         vector<string> stk;
         stringstream ss(path);
-        while(getline(ss,tmp,'/')){
-            if(tmp=="" || tmp==".") continue;
-            if(tmp==".." && !stk.empty()) stk.pop_back();
-            else if(tmp!="..") stk.push_back(tmp);
+        while(getline(ss, tmp, '/')) {
+            if(tmp == "" || tmp == ".") continue;
+            if(tmp == ".." && !stk.empty()) stk.pop_back();
+            else if(tmp != "..") stk.push_back(tmp);
         }
-        for(auto str:stk) res+="/"+str;
-        return res.empty()?"/":res;
+        for(auto str : stk) res += "/" + str;
+        return res.empty() ? "/" : res;
     }
 };
