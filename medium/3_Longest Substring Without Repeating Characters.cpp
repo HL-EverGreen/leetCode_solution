52f9c92534e087197b18daef608c658179bcecd6
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-01-19 20:24:11 -0500

    string_hash table
    string_hash table

--- medium/3_Longest Substring Without Repeating Characters.cpp
+++ medium/3_Longest Substring Without Repeating Characters.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //hash table
-        //time complexity: O(n)
+        // string & hash table
+        // time complexity: O(n), space complexity: O(n)
+        // 24ms, beats 87.78%
         
-        unordered_map<char, int> dict;
-        int max_len = 0, start = -1;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(dict.find(s[i]) != dict.end()){
-                //start can only increase
+        unordered_map<int, int> dict;
+        int start = -1, len = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(dict.find(s[i]) != dict.end()) {
                 start = max(start, dict[s[i]]);
             }
             dict[s[i]] = i;
-            max_len = max(max_len, i - start);
+            len = max(len, i - start);
         }
-        return max_len;
+        return len;
     }
 };
