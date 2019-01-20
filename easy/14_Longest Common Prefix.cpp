d53d12e629b3fb6f78650cac686798973ee6b1b5
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-01-19 20:02:45 -0500

    string
    string

--- easy/14_Longest Common Prefix.cpp
+++ easy/14_Longest Common Prefix.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {//vector_string
-        int s_size=strs.size(),count=0;
-        string res;
-        char tmp;
-        if(s_size==0) return res;
-        if(s_size==1) return strs[0];
-        while(count<strs[0].size()){
-            tmp=strs[0][count];
-            for(int i=1;i<s_size;i++){
-                if(count>=strs[i].size() || tmp!=strs[i][count]) return res;
+    string longestCommonPrefix(vector<string>& strs) {
+        // string
+        // time complexity: O(nk), k is the length of first string, space complexity: O(1)
+        // 4ms, beats 99.95%
+        
+        string prefix;
+        for(int index = 0; strs.size() > 0; prefix += strs[0][index++]) {       // index: ith character in first string
+            for(int i = 0; i < strs.size(); i++) {                              // i: string in vector
+                if(index >= strs[i].length() || (i > 0 && strs[i][index] != strs[i - 1][index])) { return prefix; }
             }
-            res+=tmp;
-            count++;
         }
-        return res;
+        return prefix;
     }
 };
