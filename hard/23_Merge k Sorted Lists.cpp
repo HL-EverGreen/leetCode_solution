7f25db3bbf6d46b9296e8eed3d4aeb0bc3034b90
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-07 12:24:39 -0500

    list_priority queue

--- hard/23_Merge k Sorted Lists.cpp
+++ hard/23_Merge k Sorted Lists.cpp
@@ -8,10 +8,47 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) { //merge list
-        //even works well when NULL exists in #lists# without evidently determine if there is NULL
+    // list & priority queue
+    // time complexity: O(nlogn), n is the total number of nodes, space complexity: O(k), k is the number of list
+    // 16ms, beats 99%
+    
+    // method 1: using priority queue
+    struct compare{
+        bool operator() (const ListNode* a, const ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() == 0) return nullptr;
+        priority_queue<ListNode*, vector<ListNode*>, compare> nextNode;
+        
+        // push head node for each list
+        int numList = lists.size();
+        for(int i = 0; i < numList; i++) {
+            if(lists[i] != nullptr) {
+                nextNode.push(lists[i]);
+            }
+        }
+        
+        // update the priority queue
+        ListNode* dummy = new ListNode(0), *cur = dummy;
+        while(!nextNode.empty()) {
+            ListNode* next = nextNode.top();
+            nextNode.pop();
+            cur->next = next;
+            cur = next;
+            if(next->next != nullptr) nextNode.push(next->next);
+        }
+        return dummy->next;
+    }
+    
+    /*
+    // method 2: divided into many merge two lists problems
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // even works well when NULL exists in #lists# without evidently determine if there is NULL
         if(lists.empty()) return nullptr;
-        while(lists.size() > 1){
+        while(lists.size() > 1) {
             lists.push_back(mergeTwoLists(lists[0], lists[1]));
             lists.erase(lists.begin());
             lists.erase(lists.begin());
@@ -19,52 +56,34 @@ public:
         return lists.front();
     }
     
-    ListNode* mergeTwoLists(ListNode *l1, ListNode *l2) { //recursive
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         if(l1 == nullptr) return l2;
         if(l2 == nullptr) return l1;
-        if(l1->val <= l2->val){
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        }
-        else{
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
-        }
-        //where is the bug??
-        /*
-        int l_size=lists.size(),i=1;
-        if(l_size==0) return nullptr;
-        else if(l_size==1) return lists[0];
-        ListNode* dummy=new ListNode(0);
-        dummy->next=lists[0];
-        while(!dummy->next){
-            dummy->next=lists[i];
-            i++;
-            if(i==l_size && !dummy->next) return nullptr;
-            else if(i==l_size) return dummy->next;
-        }
-        ListNode* l1,*l2,*prev;
-        for(;i<l_size;i++){
-            l1=dummy->next;
-            l2=lists[i];
-            while(!l2){
-                i++;
-                if(i==l_size) return dummy->next;
-                l2=lists[i];
+        
+        // iteration version
+        ListNode* newHead = new ListNode(0), *cur = newHead;
+        while(l1 && l2) {
+            if(l1->val < l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
+            } else {
+                cur->next = l2;
+                l2 = l2->next;
             }
-            prev=dummy;
-            while(l1 || l2){
-                if(!l1 || (l2&&l2->val<=l1->val)){
-                    prev->next=l2;
-                    l2=l2->next;
-                }
-                else if(!l2 || l1&&l1->val<=l2->val){
-                    prev->next=l1;
-                    l1=l1->next;
-                }
-            }
+            cur = cur->next;
         }
-        return dummy->next;
-        */
+        if(l1) cur->next = l1;
+        else cur->next = l2;
+        return newHead->next;
+        
+        // recursive version
+        // if(l1->val < l2->val) {
+        //     l1->next = mergeTwoLists(l1->next, l2);
+        //     return l1;
+        // } else {
+        //     l2->next = mergeTwoLists(l1, l2->next);
+        //     return l2;
+        // }
     }
+    */
 };
