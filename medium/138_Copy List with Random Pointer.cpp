26a6fbaa545bccbb29e5865d39bcac629bf459ae
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-06 23:40:19 -0500

    list

--- medium/138_Copy List with Random Pointer.cpp
+++ medium/138_Copy List with Random Pointer.cpp
@@ -9,36 +9,37 @@
 class Solution {
 public:
     RandomListNode *copyRandomList(RandomListNode *head) {
-        //list
-        //time complexity: O(n)
-        //first expand original list by adding same node after each node
-        //then separate the list into two new same list
-        //return the new list
+        // list
+        // time complexity: O(n), space complexity: O(n)
+        // 60ms, beats 10%, using hashmap can be faster
+        
+        // first expand original list by adding same node after each node
+        // then separate the list into two new same list
+        // return the new list
         
         RandomListNode *newHead, *l1, *l2;
-        if(!head) return NULL;
+        if(!head) return nullptr;
         
-        //expand original list by adding same node after each node
-        for(l1 = head; l1 != NULL; l1 = l1 -> next -> next){
-            l2 = new RandomListNode(l1 -> label);
-            l2 -> next = l1 -> next;
-            l1 -> next = l2;
+        // expand original list by adding same node after each node
+        for(l1 = head; l1 != nullptr; l1 = l1->next->next) {
+            l2 = new RandomListNode(l1->label);
+            l2->next = l1->next;
+            l1->next = l2;
         }
         
-        newHead = head -> next;
+        newHead = head->next;
         
-        //set random pointer
-        for(l1 = head; l1 != NULL; l1 = l1 -> next -> next){
-            if(l1 -> random != NULL) l1 -> next -> random = l1 -> random -> next;
+        // set random pointer
+        for(l1 = head; l1 != nullptr; l1 = l1->next->next) {
+            if(l1->random != nullptr) l1->next->random = l1->random->next;
         }
         
-        //seperate list
-        for(l1 = head; l1 != NULL; l1 = l1 -> next){
-            l2 = l1 -> next;
-            l1 -> next = l2 -> next;
-            if(l2 -> next != NULL) l2 -> next = l2 -> next -> next;
+        // seperate list
+        for(l1 = head; l1 != nullptr; l1 = l1->next) {
+            l2 = l1->next;
+            l1->next = l2->next;
+            if(l2->next != nullptr) l2->next = l2->next->next;
         }
-        
         return newHead;
     }
 };
