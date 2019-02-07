432bd3d1a75032a0d43da422fb25f6f146ebc3b0
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-07 12:36:37 -0500

    priority queue

--- hard/295_Find Median from Data Stream.cpp
+++ hard/295_Find Median from Data Stream.cpp
@@ -1,40 +1,33 @@
 class MedianFinder {
 public:
-    priority_queue<int> small;
-    priority_queue<int,vector<int>,greater<int>> large;
     /** initialize your data structure here. */
-    MedianFinder() { //priority_queue
+    // priority queue
+    // addNum time complexity: O(logn), findMedian time complexity: O(1)
+    MedianFinder() {
         
     }
     
+    // dynamically manage small and large queue
     void addNum(int num) {
         small.push(num);
         large.push(small.top());
         small.pop();
-        if(large.size()>small.size()){
+        if(large.size() > small.size()) {
             small.push(large.top());
             large.pop();
         }
-        /*
-        if(!large.empty() && num>large.top()) large.push(num);
-        else small.push(num);
-        while(small.size()>large.size()+1){
-            large.push(small.top());
-            small.pop();
-        }
-        while(small.size()+1<large.size()){
-            small.push(large.top());
-            large.pop();
-        }
-        */
     }
     
     double findMedian() {
-        if(small.size()>large.size()) return small.top();
-        //else if(large.size()>small.size()) return large.top();
-        else return (small.top()+large.top())/2.0;
+        if(small.size() > large.size()) return small.top();
+        else return (small.top() + large.top()) / 2.0;
     }
+private:
+    priority_queue<int> small;
+    priority_queue<int, vector<int>, greater<int>> large;
+    
 };
+
 /**
  * Your MedianFinder object will be instantiated and called as such:
  * MedianFinder obj = new MedianFinder();
