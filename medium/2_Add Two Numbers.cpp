c1cf9ca14e6f1104fa97f4a5372c87ab418820e6
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-07 20:39:27 -0500

    linked list

--- medium/2_Add Two Numbers.cpp
+++ medium/2_Add Two Numbers.cpp
@@ -8,18 +8,22 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {//linked_list
-        ListNode *dummy=new ListNode(0), *p=dummy;
-        int car=0, cur=0;
-        while(l1 || l2 || car){
-            cur=(l1?l1->val:0)+(l2?l2->val:0)+car;
-            car=cur/10;
-            cur%=10;
-            ListNode* new_node=new ListNode(cur);
-            p->next=new_node;
-            p=p->next;
-            l1=l1?l1->next:l1;
-            l2=l2?l2->next:l2;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        // linked list
+        // time complexity: O(n), space complexity: O(n) (used for generating new list)
+        // 24ms, beats 99%
+        
+        ListNode *dummy = new ListNode(0), *prev = dummy;
+        int carry = 0;
+        while(l1 || l2 || carry) {
+            int cur = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = cur / 10;
+            cur = cur % 10;
+            ListNode *temp = new ListNode(cur);
+            prev->next = temp;
+            prev = prev->next;
+            l1 = (l1 ? l1->next : nullptr);
+            l2 = (l2 ? l2->next : nullptr);
         }
         return dummy->next;
         
