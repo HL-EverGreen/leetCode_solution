8854fef8270d420b9d1fa62fd96074a507d15c65
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-08 21:27:36 -0500

    binary tree_BFS

--- medium/103_Binary Tree Zigzag Level Order Traversal.cpp
+++ medium/103_Binary Tree Zigzag Level Order Traversal.cpp
@@ -9,50 +9,53 @@
  */
 class Solution {
 public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root){ // queue
+    // binary tree & BFS
+    // 12ms, beats 100%
+
+    // method 1
+    // traditional preorder traversal method + reverse alternatively
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        traverseTree(res, root, 0);
+        for(int i = 1; i < res.size(); i += 2) reverse(res[i].begin(), res[i].end());
+        return res;
+    }
+    
+    void traverseTree(vector<vector<int>>& res, TreeNode *root, int depth) {
+        if(root == nullptr) return;
+        if(depth == res.size()) res.push_back({});
+        res[depth].push_back(root->val);
+        traverseTree(res, root->left, depth + 1);
+        traverseTree(res, root->right, depth + 1);
+    }
+
+    // method 2
+    // BFS + reverse flag, can be more faster (doesn't need to reverse every two line)
+    /*
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root){
+        // 12ms, beats 100%
         vector<vector<int>> res;
         if(!root) return res;
         queue<TreeNode*> q;
         TreeNode* tmp;
-        bool dir=true;  //save direction
+        bool dir = true;                       //save direction
         q.push(root);
         while(!q.empty()){
-            int size=q.size(),index;
+            int size = q.size(), index;
             vector<int> row(size);
-            for(int i=0;i<size;i++){
-                tmp=q.front();
+            for(int i = 0; i < size; i++){
+                tmp = q.front();
                 q.pop();
-                if(dir) index=i;        //left to right
-                else index=size-i-1;    //right to left
-                row[index]=tmp->val;
+                if(dir) index = i;             //left to right
+                else index = size - i - 1;     //right to left
+                row[index] = tmp->val;
                 if(tmp->left) q.push(tmp->left);
                 if(tmp->right) q.push(tmp->right);
             }
-            dir=!dir;
+            dir = !dir;
             res.push_back(row);
         }
         return res;
-    }
-    
-    //traditional preorder traversal method + reverse alternatively
-    /*
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> res;
-        preorderTraversal(root,res,0);
-        for(int i=0;i<res.size();i++)
-            if(i%2==1)
-                reverse(res[i].begin(),res[i].end());
-        return res;
-    }
-    
-    void preorderTraversal(TreeNode* root, vector<vector<int>>& res, int depth){
-        if(root){
-            if(res.size()==depth)
-                res.push_back(vector<int>());
-            res[depth].push_back(root->val);
-            preorderTraversal(root->left,res,depth+1);
-            preorderTraversal(root->right,res,depth+1);
-        }
     }
     */
 };
