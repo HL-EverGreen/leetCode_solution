6265074d8a30823cbcd52dcce211f6409063c45f
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-08 21:10:26 -0500

    array_sliding window

--- hard/239_Sliding Window Maximum.cpp
+++ hard/239_Sliding Window Maximum.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {//deque
-        deque<int> deq;
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // array & sliding window
+        // time complexity: O(n), space complexity: O(k)
+        // 64ms, beats 100%
+        
+        // Each round: 
+        // Remove element that out of range
+        // Remove element that does not need to be considered
+        // Add new element
+        deque<int> window;
         vector<int> res;
-        for(int i=0;i<nums.size();i++){
-            if(!deq.empty() && deq.front()==i-k) deq.pop_front(); //remove element out of current window
-            while(!deq.empty() && nums[deq.back()]<nums[i]) deq.pop_back(); //delete element which need not to be considered
-            deq.push_back(i);
-            if(i>=k-1) res.push_back(nums[deq.front()]);
+        int nSize = nums.size();
+        for(int i = 0; i < nSize; i++) {
+            if(!window.empty() && window.front() <= i - k) window.pop_front();              // remove element out of current window
+            while(!window.empty() && nums[window.back()] <= nums[i]) window.pop_back();     // delete element which need not to be considered
+            window.push_back(i);
+            if(i + 1 >= k) res.push_back(nums[window.front()]);                             // start push result at the position of the first window
         }
         return res;
     }
