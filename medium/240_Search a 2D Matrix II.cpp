10a2c190c9a5c07de395b419bcdacdb1080ef5db
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-09 16:47:13 -0500

    matrix

--- medium/240_Search a 2D Matrix II.cpp
+++ medium/240_Search a 2D Matrix II.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {//matrix
-        if(matrix.size()==0 || matrix[0].size()==0) return false;
-        int i=0,j=matrix[0].size()-1,m=matrix.size();
-        while(i<m && j>=0){                    //instead of while(i<matrix.size() && j>=0) to save running time
-            if(matrix[i][j]==target) return true;
-            else if(matrix[i][j]>target) j--;
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // matrix
+        // time complexity: O(n), n is the size of matrix
+        // space complexity: O(1)
+        // 60ms, beats 99%
+        
+        // main idea:
+        // start from upper right: 1) equal, return true
+        //                         2) less, move down
+        //                         3) greater, move left
+        
+        if(matrix.empty() || matrix[0].empty()) return false;
+        int row = matrix.size(), col = matrix[0].size(), i = 0, j = col - 1;
+        while(i < row && j >= 0) {
+            int cur = matrix[i][j];
+            if(cur == target) return true;
+            else if(cur > target) j--;
             else i++;
         }
         return false;
