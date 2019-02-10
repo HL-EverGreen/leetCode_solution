9db76f12f272f76af8fd6b91be2c8cecceae6a77
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-09 22:25:56 -0500

    string_hash table

--- easy/771_Jewels and Stones.cpp
+++ easy/771_Jewels and Stones.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int numJewelsInStones(string J, string S) {
-        int res=0;
-        for(int i=0;i<J.length();i++)
-            for(int j=0;j<S.length();j++)
-                if(J[i]==S[j])
-                    res++;
+        // string & hash table
+        // time complexity: O(m + n), m is J.length(), n is S.length()
+        // space complexity: O(n)
+        // 8ms, beats 99.33%
+        
+        // main idea:
+        // use hash table to record all jewels
+        unordered_map<char, int> count;
+        for(auto ch : S) count[ch]++;
+        int res = 0;
+        for(auto ch : J) {
+            if(count.find(ch) != count.end()) res += count[ch];
+        }
         return res;
     }
 };
