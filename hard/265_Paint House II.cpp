cafa3c23fa3d9641dd74b62f939e1f5fd136ce02
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-13 15:42:31 -0500

    DP

--- hard/265_Paint House II.cpp
+++ hard/265_Paint House II.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    int minCostII(vector<vector<int>>& costs) {//dp
-        //dp[j] represent the minimum cost when paint Jth color
-        int size_n=costs.size();
-        if(size_n==0) return 0;
-        int size_k=costs[0].size();
-        if(size_k==0) return 0;
-        vector<int> dp(size_k,0);
-        int min1_old,min2_old,min1,min2;
-        for(int i=0;i<size_n;i++){
-            min1_old=(i==0?0:min1); //min1_old: the smallest cost in (i-1) round
-            min2_old=(i==0?0:min2); //min2_old: the second smallest cost in (i-1) round
-            min1=INT_MAX;          
-            min2=INT_MAX;         
-            for(int j=0;j<size_k;j++){
-                if(dp[j]!=min1_old || min1_old==min2_old) //if the cheapest plan in last round does not ends with Jth color, choose it
-                    dp[j]=min1_old+costs[i][j];
-                else if(dp[j]==min1_old)                  //if the cheapest plan in last round ends paint with Jth color, choose the second
-                                                          // cheapest plan in the last round (to avoid adjancent house having same color)
-                    dp[j]=min2_old+costs[i][j];
-                if(min1<=dp[j]){
-                    min2=min(min2,dp[j]);
-                }else{
-                    min2=min1;
-                    min1=dp[j];
-                }
+    int minCostII(vector<vector<int>>& costs) {
+        // DP
+        // time complexity: O(nk), space complexity: O(k)
+        // 16ms, beats 100%
+        // main idea: 
+        // 1. maintain the minimal and second minimal value in each round (v1 and v2)
+        // 2. if the cheapest plan in last round does not ends with Jth color, choose it
+        // 3. if the cheapest plan in last round ends paint with Jth color, choose the second
+        // cheapest plan in the last round (to avoid adjancent house having same color)
+        // res[i] represents cheapest plan in each round while painting last house with ith color
+        
+        if(costs.empty() || costs[0].empty()) return 0;
+        int n = costs.size(), k = costs[0].size(), v1 = INT_MAX, v2 = INT_MAX;
+        vector<int> res(k, 0);
+
+        for(int i = 0; i < n; i++) {
+            int old_v1 = (i == 0 ? 0 : v1), old_v2 = (i == 0 ? 0 : v2);
+            v1 = INT_MAX, v2 = INT_MAX;
+            for(int j = 0; j < k; j++) {
+                if(res[j] == old_v1 && old_v1 != old_v2) res[j] = costs[i][j] + old_v2;
+                else res[j] = costs[i][j] + old_v1;
+                if(res[j] < v1) {
+                    v2 = v1;
+                    v1 = res[j];
+                } else if(res[j] < v2) 
+                    v2 = res[j];
             }
         }
-        return min1;
-        
+        return v1;
     }
 };
