98d6065edbf23535ac9b2273cc0a0452717f91c5
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-12 23:00:11 -0500

    array_two pointers

--- easy/283_Move Zeroes.cpp
+++ easy/283_Move Zeroes.cpp
@@ -3,11 +3,22 @@ public:
     void moveZeroes(vector<int>& nums) {
         // array
         // time complexity: O(n), space complexity: O(1)
-        // 16ms, beats 45.29%
+        // 16ms, beats 100%
         
         // method 1
+        // two pointers, use left to denote replacable element (0 or equal with right)
+        // use right to check each element
+        int left = 0, right = 0;
+        while(right < nums.size()) {
+            if(nums[right] != 0) swap(nums[left++], nums[right++]);
+            else right++;
+        }
+
+
+        // method 2
         // use start to fill first part of vector to be non-zero
         // then fill the rest to zero
+        /*
         int start = 0;
         for(auto num:nums) {
             if(num != 0) {
@@ -18,17 +29,8 @@ public:
         for(int i = start; i < nums.size(); i++) { 
             nums[i] = 0;
         }
-        
-        // method 2
-        // kind of bubble sort, but skip all continous 0
-        // 16ms, beats 45.29%
-        /*
-        int start = 0, end = 0;
-        while(end < nums.size()) {
-            while(nums[end] == 0 && end < nums.size() - 1) { end++; }
-            swap(nums[start++], nums[end++]);
-        }
         */
+        
 
         // method 3:
         // use erase and push_back
