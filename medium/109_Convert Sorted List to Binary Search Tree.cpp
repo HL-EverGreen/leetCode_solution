052dcbd3fa7651235dd6ee2f1109f1f6a8cca503
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-13 20:55:48 -0500

    linked list to BST

--- medium/109_Convert Sorted List to Binary Search Tree.cpp
+++ medium/109_Convert Sorted List to Binary Search Tree.cpp
@@ -18,23 +18,31 @@
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
-        return sortedListToBST(head,NULL);
+        // linked list to BST
+        // time complexity: O(nlogn), space complexity: O(1) + stack memory
+        // 40ms, beats 100%
+        
+        // follow up: use array to store value in lists first can reduce time complexity to O(n) : 
+        // 1) O(n) to convert to array
+        // 2) O(logn) to generate BST, since only need O(1) to find the middle node
+        
+        return sortedListToBST(head, nullptr);
     }
     
-    TreeNode* sortedListToBST(ListNode* head, ListNode* tail){
-        if(head==tail) return NULL;
-        if(head->next==tail) return new TreeNode(head->val); //directly return #new TreeNode(head->val)# instead the following 
-                                                             //to save running time
-            //TreeNode* root=new TreeNode(head->val);   
-            //return root;
-        ListNode *mid=head, *tmp=head;
-        while(tmp!=tail && tmp->next!=tail){ //find the mid node
-            mid=mid->next;
-            tmp=tmp->next->next;
+    TreeNode* sortedListToBST(ListNode* head, ListNode* tail) {
+        if(head == tail) return nullptr;
+        if(head->next == tail) return new TreeNode(head->val);              // directly return #new TreeNode(head->val)# instead the following 
+                                                                            // to save running time
+        
+        ListNode* fast = head, *mid = head;
+        while(fast != tail && fast->next != tail) {                         // find the middle node
+            fast = fast->next->next;
+            mid = mid->next;
         }
-        TreeNode* root=new TreeNode(mid->val);
-        root->left=sortedListToBST(head,mid);
-        root->right=sortedListToBST(mid->next,tail);
+        
+        TreeNode* root = new TreeNode(mid->val);
+        root->left = sortedListToBST(head, mid);
+        root->right = sortedListToBST(mid->next, tail);
         return root;
     }
 };
