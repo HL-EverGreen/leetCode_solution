3ccbc7bc5dcbce3ca701fac719faca071bd6d077
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-20 00:37:07 -0500

    binary tree

--- hard/124_Binary Tree Maximum Path Sum.cpp
+++ hard/124_Binary Tree Maximum Path Sum.cpp
@@ -9,17 +9,23 @@
  */
 class Solution {
 public:
-    int maxPathSum(TreeNode* root) { //binary tree
-        int res=INT_MIN;
-        findMaxPath(root,res);
+    int maxPathSum(TreeNode* root) {
+        // binary tree
+        // time complexity: O(n), space complexity: O(1)
+        // 40ms, beats 99.5%
+        
+        // Main idea:
+        // When entering a node, compare value containing this node's value to the res (since its children value has already been compared to the res)
+        int res = INT_MIN;
+        maxPathSum(root, res);
         return res;
     }
     
-    int findMaxPath(TreeNode* root, int& res){
+    int maxPathSum(TreeNode* root, int& res) {
         if(!root) return 0;
-        int left=max(0,findMaxPath(root->left, res));
-        int right=max(0,findMaxPath(root->right, res));
-        res=max(res, left+right+root->val);  //比较每一个当该结点作为父节点时最大路径和的值(带左路径或右路径或都带或都不带)
-        return max(left, right)+root->val;   //返回包含该节点的一个路径(且该节点为路径的端点)
+        int left = max(0, maxPathSum(root->left, res));
+        int right = max(0, maxPathSum(root->right, res));
+        res = max(res, root->val + left + right);                   // 比较每一个当该结点作为父节点时最大路径和的值(带左路径或右路径或都带或都不带)
+        return root->val + max(left, right);                        // 返回包含该节点的一个路径(且该节点为路径的端点)
     }
 };
