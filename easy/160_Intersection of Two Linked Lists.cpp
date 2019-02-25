ca00a5c6d811324534fe9f21d1655b49efad2d02
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-25 14:09:44 -0500

    linked list

--- easy/160_Intersection of Two Linked Lists.cpp
+++ easy/160_Intersection of Two Linked Lists.cpp
@@ -8,15 +8,23 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) { //linked list
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // linked list
+        // time complexity: O(n), space complexity: O(1)
+        // 52ms, beats 98.07%
+        
+        // main idea:
+        // When p1 reach at end of A, set p1 = B's head. Same as p2
+        // So if exists interesction, the total distance would be same and p1 p2 will reach the intersection simultaneously.
+        // If doesn't exist, p1 and p2 will reach NULL simultaneously
         if(!headA || !headB) return nullptr;
-        ListNode *p1=headA, *p2=headB;
-        while(p1 && p2 && p1!=p2){
-            p1=p1->next;
-            p2=p2->next;
-            if(p1==p2) return p1;
-            if(!p1) p1=headB;   //when one pointer come at the end of one linked list, change to another
-            if(!p2) p2=headA;   //so that when they both come to collision node, the distance is same (or no intersection they come the NULL at the same time)
+        ListNode *p1 = headA, *p2 = headB;
+        while(p1 && p2 && p1 != p2) {
+            p1 = p1->next;
+            p2 = p2->next;
+            if(p1 == p2) return p1;
+            if(!p1) p1 = headB;
+            if(!p2) p2 = headA;
         }
         return p1;
     }
