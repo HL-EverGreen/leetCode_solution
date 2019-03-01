26fec8e931ba9cf4f346a150e0ede38070b9c250
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-02-28 23:05:21 -0500

    BST

--- easy/235_Lowest Common Ancestor of a Binary Search Tree.cpp
+++ easy/235_Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -7,17 +7,19 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {//recursive
+class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return findLowestCommonAncestor(root,p,q);
-    }
-    
-    TreeNode* findLowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
-        if(p->val<root->val && q->val<root->val)
-            return findLowestCommonAncestor(root->left,p,q);
-        else if(p->val>root->val && q->val>root->val)
-            return findLowestCommonAncestor(root->right,p,q);
+        // BST
+        // time complexity: O(n), space complexity: O(1) + stack memory
+        // 44ms, beats 97.9%
+        
+        // main idea:
+        // find first root whose val is between q's val and p's val
+        int root_val = root->val, p_val = p->val, q_val = q->val;
+        if(root_val > p_val && root_val > q_val) return lowestCommonAncestor(root->left, p, q);
+        else if(root_val < p_val && root_val < q_val) return lowestCommonAncestor(root->right, p, q);
         else return root;
+        
     }
 };
