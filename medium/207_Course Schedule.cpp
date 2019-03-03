3ce06d4ccf55dd7bc68446c66c40482578dabc1c
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-03-03 12:59:48 -0500

    DFS_BFS_graph

--- medium/207_Course Schedule.cpp
+++ medium/207_Course Schedule.cpp
@@ -1,15 +1,51 @@
 class Solution {
-    //dfs
-    
-    //For DFS, it will first visit a node, then one neighbor of it, then one neighbor of this neighbor... and so on. 
-    //If it meets a node which was visited in the current process of DFS visit, a cycle is detected and we will return false.
-    //Otherwise it will start from another unvisited node and repeat this process till all the nodes have been visited. 
-    //Note that you should make two records: one is to record all the visited nodes and the other 
-    //is to record the visited nodes in the current DFS visit.
+public:
+    // BFS & DFS & graph
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        // method 1:
+        // BFS
+        // time complexity: O(n^2), space complexity: O(n)
+        // 20ms, beats 100%
+        
+        // main idea:
+        // Use queue to store nodes whose in degree is 0
+        // When pop queue's top, check its successor (in vector<unordered_set<int>>), and reduce their in degree by 1. 
+        // If some's in degree becomes 0, push it in queue
+        // Check number of nodes in total being pushed in queue, whether == numCourses
 
-    //The code is as follows. We use a vector<bool> visited to record all the visited nodes and another vector<bool> onpath 
-    //to record the visited nodes of the current DFS visit. Once the current visit is finished, we reset the onpath value 
-    //of the starting node to false.
+        vector<unordered_set<int>> pre(numCourses);
+        vector<int> inDeg(numCourses);
+        for(auto& p : prerequisites) {                  // construct relationship
+            pre[p.first].insert(p.second);
+            inDeg[p.second]++;
+        }
+        
+        queue<int> q;
+        int num = 0;
+        for(int i = 0; i < numCourses; i++) {           // push nodes with 0 in-degree into queue
+            if(inDeg[i] == 0) {
+                q.push(i);
+                num++;
+            }
+        }
+        
+        while(!q.empty()) {                             // continue check successor for nodes in queue
+            int cur = q.front();
+            q.pop();
+            for(auto& i : pre[cur]) {
+                if(--inDeg[i] == 0) {
+                    q.push(i);
+                    num++;
+                }
+            }
+        }
+        return num == numCourses;
+    }
+
+    // method 2:
+    // DFS
+    // main idea: detect if there exists a cycle. If exists, then can't schedule courses
+    /*
 public:
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
         vector<unordered_set<int>> graph = make_graph(numCourses, prerequisites);
@@ -36,12 +72,15 @@ private:
                 return true;
         return onpath[node] = false;
     }
-    
-    //bfs
-    //BFS uses the indegrees of each node. We will first try to find a node with 0 indegree. 
-    //If we fail to do so, there must be a cycle in the graph and we return false. Otherwise we have found one. 
-    //We set its indegree to be -1 to prevent from visiting it again and reduce the indegrees of all its neighbors by 1. 
-    //This process will be repeated for n (number of nodes) times. If we have not returned false, we will return true.
+    */
+
+
+    // Another kind of BFS, use array
+
+    // BFS uses the indegrees of each node. We will first try to find a node with 0 indegree. 
+    // If we fail to do so, there must be a cycle in the graph and we return false. Otherwise we have found one. 
+    // We set its indegree to be -1 to prevent from visiting it again and reduce the indegrees of all its neighbors by 1. 
+    // This process will be repeated for n (number of nodes) times. If we have not returned false, we will return true.
     /*
 public:
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
