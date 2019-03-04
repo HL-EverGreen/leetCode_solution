a2aae906f89f91eb4bdd4a84f0cae4fe35db5075
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-03-04 12:58:56 -0500

    DFS_DP

--- hard/996_Number of Squareful Arrays.cpp
+++ hard/996_Number of Squareful Arrays.cpp
@@ -27,3 +27,65 @@ public:
         return res;
     }
 };
+
+
+// DP
+// time complexity: O(2^n * n^2)
+// space complexity: O(2^n * n)
+// 思路见：https://www.acwing.com/solution/leetcode/content/1034/
+/*
+class Solution {
+public:
+
+    bool isSquare(int x) {
+        int t = sqrt(x);
+        return t * t == x;
+    }
+
+    int fact(int x) {
+        int t = 1;
+        for (int i = 2; i <= x; i++)
+            t *= i;
+        return t;
+    }
+
+    int numSquarefulPerms(vector<int>& A) {
+        int n = A.size();
+
+        sort(A.begin(), A.end());
+
+        vector<int> s;
+        int cnt = 1;
+        for (int i = 1; i < n; i++) {
+            if (A[i] != A[i - 1]) {
+                s.push_back(cnt);
+                cnt = 0;
+            }
+            cnt++;
+        }
+        s.push_back(cnt);
+
+        vector<vector<int>> f(1 << n, vector<int>(n, 0));
+
+        for (int i = 0; i < n; i++)
+            f[1 << i][i] = 1;
+
+        for (int S = 1; S < 1 << n; S++)
+            for (int i = 0; i < n; i++)
+                if (S & (1 << i))
+                    for (int j = 0; j < n; j++)
+                        if (!(S & (1 << j)) && isSquare(A[i] + A[j])) {
+                            f[S | (1 << j)][j] += f[S][i];
+                        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            ans += f[(1 << n) - 1][i];
+
+        for (int i = 0; i < s.size(); i++)
+            ans /= fact(s[i]);
+
+        return ans;
+    }
+};
+*/
