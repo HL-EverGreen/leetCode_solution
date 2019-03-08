bf6f294cb7a5acdc4156da159060aa005e816a2d
HL-EverGreen <haoranl2@andrew.cmu.edu>
2019-03-07 19:08:36 -0500

    string

--- easy/242_Valid Anagram.cpp
+++ easy/242_Valid Anagram.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {//actually, like alphabetic list we can simply use array instead of map,
-                                        //to save the time consumption spent on every *find()* operation
+    bool isAnagram(string s, string t) {
+        // string
+        // time complexity: O(n), space complexity: O(n)
+        // 12ms, beats 98.03%
+        
+        if(s.length() != t.length()) return false;
+        vector<int> dict(26);
+        for(auto& ch : s) dict[ch - 'a']++;
+        for(auto& ch : t) if(--dict[ch - 'a'] < 0) return false;
+        return true;
+
+        // first step :)
+        
+        //actually, like alphabetic list we can simply use array instead of map,
+        //to save the time consumption spent on every *find()* operation
+
+        /*
         map<char,int> myMap;
         if(s.length()==0&&t.length()==0) return true;
         if(s.length()==0||t.length()==0) return false;
@@ -25,5 +40,6 @@ public:
                 return false;
         }
         return true;
+        */
     }
 };
