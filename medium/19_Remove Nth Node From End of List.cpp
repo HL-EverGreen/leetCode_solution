dd0d79de7c5334dd0547d8712049f1154ca4abee
haoran.li <lhr449307680@gmail.com>
2019-07-07 16:45:58 -0700

    linked list

--- medium/19_Remove Nth Node From End of List.cpp
+++ medium/19_Remove Nth Node From End of List.cpp
@@ -8,7 +8,25 @@
  */
 class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {//linked_list
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        // Linked_list
+        // time complexity: O(n), space complexity: O(1)
+        // 4ms, beats 91.64%
+        // Use dummy head version
+        ListNode *dummy = new ListNode(0), *prev = dummy, *second = dummy;
+        dummy->next = head;
+        while(n--) {
+            second = second->next;
+        }
+        while(second->next) {
+            prev = prev->next;
+            second = second->next;
+        }
+        prev->next = prev->next->next;
+        return dummy->next;
+
+        // Use ** version, 取地址做法相当于dummy
+        /*
         ListNode **l1=&head, *l2=head;
         for(int i=1;i<n;i++){ //set l2 in ahead of l1 within n-1 steps, so when l2 reaches end, l1 reaches the Nth node from end of list
             if(l2->next) l2=l2->next;
@@ -20,5 +38,6 @@ public:
         }
         *l1=(*l1)->next;
         return head;
+        */
     }
 };
