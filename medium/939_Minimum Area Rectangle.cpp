2aee9da31ae8c9f0ff12a0c24e7cf9ff3edb657c
haoran.li <lhr449307680@gmail.com>
2019-08-06 22:44:59 -0700

    geometry & hash table & set

--- medium/939_Minimum Area Rectangle.cpp
+++ medium/939_Minimum Area Rectangle.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int minAreaRect(vector<vector<int>>& points) {
-        // array & set
-        // time complexity: O(n^2), space complexity: O(n)
+        // geometry & hash table & set
+        // time complexity: O(n^2) or (n^1.5), space complexity: O(n), n is the number of points
         // 140ms, beats 77.61%
         
-        // group all points according to their x coords, use set to store this points
-        // implement intersection operation between two sets, find common y coords
-        // compute each rectangle's area
+        // Group all points according to their x coords, use set to store this points.
+        // Implement intersection operation between two sets, find common y coords.
+        // Since set is in order, we just need to compare res with `width` * (same_y[k] - same_y[k - 1])
         
         // new functions:
         // set_intersection: Constructs a sorted range beginning in the location pointed by OutputIterator with the set intersection of the two sorted ranges [first1,last1) and [first2,last2).
@@ -23,7 +23,7 @@ public:
             for(auto j = next(i); j != x.end(); j++) {
                 if(j->second.size() < 2) { continue; }
                 vector<int> y;
-                set_intersection(i->second.begin(), i->second.end(), j->second.begin(), j->second.end(), inserter(y, y.begin()));
+                set_intersection(i->second.begin(), i->second.end(), j->second.begin(), j->second.end(), inserter(y, y.begin()));   // Or back_inserter(y)
                 for(int k = 1; k < y.size(); k++) {
                     res = min(res, abs(j->first - i->first) * (y[k] - y[k - 1]));
                 }
