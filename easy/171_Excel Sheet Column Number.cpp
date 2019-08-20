c6a3f8d19d892b3e528c3386b4c3888b10ef06ca
haoran.li <lhr449307680@gmail.com>
2019-08-20 11:24:47 +0800

    string

--- easy/171_Excel Sheet Column Number.cpp
+++ easy/171_Excel Sheet Column Number.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int titleToNumber(string s) {
+        // string
+        // time complexity: O(n), space complexity: O(1)
+        // 0ms, beats 100%
+        
         int res=0;
-        int temp=1;
+        long temp=1;
         if(s.length()==0) return res;
-        for(int i=s.length()-1;i>=0;i--){
-            res+=temp*(__toascii(s[i])-64); //char to ascii
-            temp*=26;
+        for(int i = s.length() - 1; i >= 0; i--){
+            res += temp * (s[i] - 'A' + 1);
+            temp *= 26;
         }
         return res;
     }
