e4928cece3fb37f5633b046d0294b130c1a2cb86
haoran.li <lhr449307680@gmail.com>
2019-09-09 22:26:29 -0400

    string & DFS

--- hard/753_Cracking the Safe.cpp
+++ hard/753_Cracking the Safe.cpp
@@ -2,34 +2,39 @@ class Solution {
 public:
     string crackSafe(int n, int k) {
         // string & DFS
-        // 8ms, beats 94.37%
+        // time complexity: O(k^n), space complexity: O(k^n) + stack memory
+        // 8ms, beats 85.93%
         
+        // And just use DFS to try every possibility
         // Each time visit a n-length password, next time visit password = current password.substr(1) + new_char in range(0, k)
         // The minimum length of total password is k^n + n - 1, when password reaches this length, stop and return
 
-        // string.erase(pos, len), Erases the portion of the string value that begins at the character position pos and spans len characters (or until the end of the string, if either the content is too short or if len is string::npos.
-        string pwd = "", cur = "";
-        for(int i = 0; i < n; i++) { cur += '0';  pwd = cur; }
-        int max_num = pow(k, n) + n - 1;
-        unordered_set<string> visited;
-        visited.insert(cur);
-        if(findMinPwd(visited, pwd, cur, max_num, n, k)) { return pwd; }
-        else return "ERROR!";
+        // *Note: string.erase(pos, len), Erases the portion of the string value that begins at the character position pos and spans len characters (or until the end of the string, if either the content is too short or if len is string::npos.
+        
+        string cur = string(n, '0');
+        string pwd = cur;
+        unordered_set<string> used;
+        int max_len = pow(k, n) + n - 1;
+        
+        used.insert(cur);
+        if(findMinPwd(used, pwd, cur, max_len, k)) return pwd;
+        else return "ERROR!"; // Can't reach here
     }
     
-    bool findMinPwd(unordered_set<string>& visited, string& pwd, string& cur, int max_num, int n, int k) {
-        if(pwd.length() == max_num) { return true; }
-        string next = cur.substr(1);
-        for(char ch = '0'; ch < '0' + k; ch++) {
-            next += ch;
-            if(visited.find(next) == visited.end()) {
-                pwd += ch;
-                visited.insert(next);
-                if(findMinPwd(visited, pwd, next, max_num, n, k)) { return true; }
-                visited.erase(next);
-                pwd = pwd.erase(pwd.length() - 1);
+    /* Do dfs based on `cur` string, try every possible `next` string */
+    bool findMinPwd(unordered_set<string>& used, string& pwd, string& cur, int max_len, int k) {
+        if(pwd.length() == max_len) return true;
+        string base = cur.substr(1);
+        for(char ch = '0'; ch < k + '0'; ++ch) {
+            string next = base + ch;     // Candidates
+
+            if(used.count(next) == 0) {
+                pwd.push_back(ch);
+                used.insert(next);
+                if(findMinPwd(used, pwd, next, max_len, k)) return true;
+                used.erase(next);
+                pwd.pop_back();
             }
-            next = next.erase(next.length() - 1);
         }
         return false;
     }
