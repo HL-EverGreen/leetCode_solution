8f783e5c3b67bd8d8bc8547218681a814d353ead
haoran.li <lhr449307680@gmail.com>
2019-09-29 14:50:51 -0400

    DFS + memo

--- medium/241_Different Ways to Add Parentheses.cpp
+++ medium/241_Different Ways to Add Parentheses.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     vector<int> diffWaysToCompute(string input) {
+        // DFS + memo
+        // time complexity: O(n^2), space complexity: O(n^2)
+        // 0ms, beats 100%
+        
+        // Main idea:
+        // For each string, try on each operator(+, -, *) by first calculating both sides and finally calculate this operator.
+        // Use memo to cache the calculated result for specific string.
+        
+        if(memo.count(input)) return memo[input];
+        
         vector<int> res;
-        for(int i=0;i<input.size();i++){
-            char cur=input[i];
-            if(cur=='+'||cur=='-'||cur=='*'){
-                vector<int>res1=diffWaysToCompute(input.substr(0,i)); //recursive
-                vector<int>res2=diffWaysToCompute(input.substr(i+1));
-                for(auto i:res1){
-                    for(auto j:res2){
-                        switch(cur){
-                            case '+':res.push_back(i+j); break;
-                            case '-':res.push_back(i-j); break;
-                            case '*':res.push_back(i*j); 
+        for(int i = 0; i < input.length(); ++i) {
+            if(input[i] == '+' || input[i] == '-' || input[i] == '*') {
+                vector<int> left = diffWaysToCompute(input.substr(0, i));   // Left side
+                vector<int> right = diffWaysToCompute(input.substr(i + 1)); // Right side
+                for(int& l : left) {
+                    for(int& r : right) {
+                        switch(input[i]) {
+                            case '+': res.push_back(l + r); break;
+                            case '-': res.push_back(l - r); break;
+                            case '*': res.push_back(l * r);
                         }
                     }
                 }
             }
         }
-        if(res.empty())
-            res.push_back(atoi(input.c_str()));
-        return res;
+        
+        if(res.empty()) res.push_back(stoi(input)); // Only contains digit
+        return memo[input] = res;
     }
+private:
+    unordered_map<string, vector<int>> memo;
 };
