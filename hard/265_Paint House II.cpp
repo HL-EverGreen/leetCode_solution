331666f716a1cb216b5aaeaa850ccc898f637cec
haoran.li <lhr449307680@gmail.com>
2019-10-17 21:01:05 -0400

    DP

--- hard/265_Paint House II.cpp
+++ hard/265_Paint House II.cpp
@@ -3,7 +3,7 @@ public:
     int minCostII(vector<vector<int>>& costs) {
         // DP
         // time complexity: O(nk), space complexity: O(k)
-        // 16ms, beats 100%
+        // 8ms, beats 100%
         // main idea: 
         // 1. maintain the minimal and second minimal value in each round (v1 and v2)
         // 2. if the cheapest plan in last round does not ends with Jth color, choose it
@@ -12,20 +12,20 @@ public:
         // res[i] represents cheapest plan in each round while painting last house with ith color
         
         if(costs.empty() || costs[0].empty()) return 0;
-        int n = costs.size(), k = costs[0].size(), v1 = INT_MAX, v2 = INT_MAX;
-        vector<int> res(k, 0);
-
-        for(int i = 0; i < n; i++) {
-            int old_v1 = (i == 0 ? 0 : v1), old_v2 = (i == 0 ? 0 : v2);
-            v1 = INT_MAX, v2 = INT_MAX;
-            for(int j = 0; j < k; j++) {
-                if(res[j] == old_v1 && old_v1 != old_v2) res[j] = costs[i][j] + old_v2;
-                else res[j] = costs[i][j] + old_v1;
-                if(res[j] < v1) {
+        int N = costs.size(), K = costs[0].size();
+        int old_v1, old_v2, v1 = 0, v2 = 0;
+        vector<int> total(K, 0);
+        
+        for(int i = 0; i < N; ++i) {    // Iterate through N house
+            old_v1 = v1; old_v2 = v2;
+            v1 = INT_MAX; v2 = INT_MAX;
+            for(int j = 0; j < K; ++j) { // Calculate each plan if paint house i with color j
+                if(old_v1 == total[j] && old_v1 != old_v2) total[j] = old_v2 + costs[i][j]; // If last cheapest plan is also painting with color j
+                else total[j] = old_v1 + costs[i][j];
+                if(total[j] < v1) {
                     v2 = v1;
-                    v1 = res[j];
-                } else if(res[j] < v2) 
-                    v2 = res[j];
+                    v1 = total[j];
+                } else if(total[j] < v2) v2 = total[j];
             }
         }
         return v1;
