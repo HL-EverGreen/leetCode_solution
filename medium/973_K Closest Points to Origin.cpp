ad31d216634f93eebd7bd08936dccc8c9a2fe8a5
haoran.li <lhr449307680@gmail.com>
2019-10-22 17:43:21 -0400

    priority queue & quick sort

--- medium/973_K Closest Points to Origin.cpp
+++ medium/973_K Closest Points to Origin.cpp
@@ -25,11 +25,11 @@ public:
         }
         return res;
         
-        
+
         // Method 2: Use native quick selection sout
         // time complexity: O(NlogK), space complexity: O(1)
         // 204ms, beats 95.27%
-        nth_element(points.begin(), points.begin() + K, points.end(), [](vector<int>& p, vector<int>& q) {
+        nth_element(points.begin(), points.begin() + K - 1, points.end(), [](vector<int>& p, vector<int>& q) {
             return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
         });
         return vector<vector<int>>(points.begin(), points.begin() + K);
