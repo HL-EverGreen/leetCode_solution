6ffa754d5cc93ccfa33383e965eea0d3c4768b09
haoran.li <lhr449307680@gmail.com>
2019-11-05 16:24:35 -0500

    bit manipulation

--- easy/231_Power of Two.cpp
+++ easy/231_Power of Two.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        //or use bit operation
-        //return ( n > 0 && ( n & ( n - 1 ) ) == 0 );
+        // bit manipulation
+        // time complexity: O(1), space complexity: O(1)
+        // 0ms, beats 100%
         
+        // Just remove last 1 in bit representation and check whether it is 0
+        if(n <= 0) return false;
+        return (n & (n - 1)) == 0;
+
+        
+        // First step :) Good job haoran! 
+        /*
         if(n<=0) return false;
         while(n!=1){
             if(n%2!=0) return false;
             n/=2;
         }
         return true;
+        */
     }
 };
