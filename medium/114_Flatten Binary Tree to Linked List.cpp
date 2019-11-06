ca2663bb04610199c926d0f62c99683d8fc4d68a
haoran.li <lhr449307680@gmail.com>
2019-11-06 15:20:10 -0500

    binary tree

--- medium/114_Flatten Binary Tree to Linked List.cpp
+++ medium/114_Flatten Binary Tree to Linked List.cpp
@@ -9,27 +9,36 @@
  */
 class Solution {
 public:
+    TreeNode* prev;
     void flatten(TreeNode* root) {
-        queue<TreeNode*> q;
-        preorderTraversal(root,q);
-        if(root){
-            TreeNode* prev=q.front();
-            q.pop();
-            while(!q.empty()){ //reconnect each node into linked list 
-                TreeNode* cur=q.front();
-                q.pop();
-                prev->right=cur;
-                prev->left=NULL;
-                prev=cur;
-            }    
-        }
-    }
-    
-    void preorderTraversal(TreeNode* root, queue<TreeNode*>& q){//push all node preorderly in queue
-        if(root){
-            q.push(root);
-            preorderTraversal(root->left,q);
-            preorderTraversal(root->right,q);
+        // binary tree
+
+        // recursive
+        // time complexity: O(n), space complexity: O(1) + stack memory
+        // 4ms, beats 93.37%
+        
+        // reverse preorder traversal, set prev and update continually
+        if(!root) return;
+        flatten(root->right);
+        flatten(root->left);
+        root->right = prev;
+        root->left = nullptr;
+        prev = root;
+        
+        // iterative
+        // time complexity: O(n), space complexity: O(1)
+        // 4ms, beats 93.37%
+        TreeNode* cur = root;
+        while(cur) {
+            if(cur->left) { // If has left subtree, find rightmost node and set its right child to cur->right. 
+                            // Then adjust cur->left to cur->right.
+                TreeNode* prev = cur->left;
+                while(prev->right) prev = prev->right;
+                prev->right = cur->right;
+                cur->right = cur->left;
+                cur->left = nullptr;
+            }
+            cur = cur->right;
         }
     }
 };
